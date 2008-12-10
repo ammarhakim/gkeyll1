66691112a8564e8d8dd752dce06f17862526d7d1
a.hakim777 <a.hakim777@localhost>
2008-12-10 22:25:52 +0000

    More tests. Now ensures that copy and assignment work for all public calls.

--- unit/lckeyval.cxx
+++ unit/lckeyval.cxx
@@ -30,6 +30,37 @@ test_a_kv(const Lucee::KeyVal& kv)
   LC_ASSERT("Testing if humbug exists", kv.has("Humbug") == false);
   LC_RAISES("Testing if runtime is raised", kv.get<int>("Humbug"), Lucee::Except);
   LC_RAISES("Testing if bad_cast is raised", kv.get<double>("meqn"), bad_cast);
+
+
+  std::vector<std::string>::const_iterator itr;
+  std::vector<std::string> names;
+
+  names = kv.getKeys<int>();
+  LC_ASSERT("Testing name of first integer", names[0] == "meqn");
+  LC_ASSERT("Testing name of first integer", names[1] == "int_a");
+  LC_ASSERT("Testing name of second integer", names[2] == "int_b");
+  LC_ASSERT("Testing name of third integer", names[3] == "int_c");
+
+  names = kv.getKeys<double>();
+  LC_ASSERT("Testing name of first double", names[0] == "gas_gamma");
+  LC_ASSERT("Testing name of first double", names[1] == "double_a");
+  LC_ASSERT("Testing name of second double", names[2] == "double_b");
+  LC_ASSERT("Testing name of third double", names[3] == "double_c");
+
+  std::vector<int> newValues 
+    = kv.get<std::vector<int> >("intList");
+  LC_ASSERT("Testing if size of returned array is correct",
+    newValues.size() == 3);
+  LC_ASSERT("Testing if elements of returned array are correct",
+    newValues[0] == 1);
+  LC_ASSERT("Testing if elements of returned array are correct",
+    newValues[1] == 2);
+  LC_ASSERT("Testing if elements of returned array are correct",
+    newValues[2] == 3);
+
+  names = kv.getKeys<std::vector<int> >();
+  LC_ASSERT("Testing if names are proper",
+    names[0] == "intList");
 }
 
 void
@@ -48,21 +79,6 @@ test_lckv_a()
   LC_RAISES("Testing if runtime is raised", kv.get<int>("Humbug"), Lucee::Except);
   LC_RAISES("Testing if bad_cast is raised", kv.get<double>("meqn"), bad_cast);
 
-  // make a copy
-  Lucee::KeyVal kv_a(kv);
-  // test if it is properly made
-  test_a_kv(kv_a);
-
-  Lucee::KeyVal kv_b;
-  kv_b = kv_a;
-  test_a_kv(kv_b);
-}
-
-void
-test_lckv_b()
-{
-  Lucee::KeyVal kv;
-
   kv.add("int_a", 1);
   kv.add("int_b", 2);
   kv.add("int_c", 3);
@@ -75,20 +91,17 @@ test_lckv_b()
   std::vector<std::string> names;
 
   names = kv.getKeys<int>();
-  LC_ASSERT("Testing name of first integer", names[0] == "int_a");
-  LC_ASSERT("Testing name of second integer", names[1] == "int_b");
-  LC_ASSERT("Testing name of third integer", names[2] == "int_c");
+  LC_ASSERT("Testing name of first integer", names[0] == "meqn");
+  LC_ASSERT("Testing name of first integer", names[1] == "int_a");
+  LC_ASSERT("Testing name of second integer", names[2] == "int_b");
+  LC_ASSERT("Testing name of third integer", names[3] == "int_c");
 
   names = kv.getKeys<double>();
-  LC_ASSERT("Testing name of first double", names[0] == "double_a");
-  LC_ASSERT("Testing name of second double", names[1] == "double_b");
-  LC_ASSERT("Testing name of third double", names[2] == "double_c");
-}
+  LC_ASSERT("Testing name of first double", names[0] == "gas_gamma");
+  LC_ASSERT("Testing name of first double", names[1] == "double_a");
+  LC_ASSERT("Testing name of second double", names[2] == "double_b");
+  LC_ASSERT("Testing name of third double", names[3] == "double_c");
 
-void
-test_lckv_c()
-{
-  Lucee::KeyVal kv;
   std::vector<int> values;
   values.push_back(1);
   values.push_back(2);
@@ -107,10 +120,18 @@ test_lckv_c()
   LC_ASSERT("Testing if elements of returned array are correct",
     newValues[2] == 3);
 
-  std::vector<std::string> names 
-    = kv.getKeys<std::vector<int> >();
+  names = kv.getKeys<std::vector<int> >();
   LC_ASSERT("Testing if names are proper",
     names[0] == "intList");
+
+  // make a copy
+  Lucee::KeyVal kv_a(kv);
+  // test if it is properly made
+  test_a_kv(kv_a);
+
+  Lucee::KeyVal kv_b;
+  kv_b = kv_a;
+  test_a_kv(kv_b);
 }
 
 int
@@ -118,7 +139,5 @@ main(void)
 {
   LC_BEGIN_TESTS("lckv_tests");
   test_lckv_a();
-  test_lckv_b();
-  test_lckv_c();
   LC_END_TESTS;
 }
