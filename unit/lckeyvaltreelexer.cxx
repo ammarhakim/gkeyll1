fdea2fe4907021251075b01f075311215c7bc98b
Ammar <a.hakim777@gmail.com>
2008-12-11 16:52:24 +0000

    Added tests for integers, reals, strings and list of integers.

--- unit/lckeyvaltreelexer.cxx
+++ unit/lckeyvaltreelexer.cxx
@@ -46,10 +46,101 @@ test_a()
     lexer.YYLex() == Lucee::LC_RIGHT_ANGLE);
 }
 
+void
+test_b()
+{
+  std::istringstream is1("val = 22");
+  Lucee::KeyValTreeLexer<float> lex1(is1);
+
+  LC_ASSERT("Testing token in input stream",
+    lex1.YYLex() == Lucee::LC_ID);
+  LC_ASSERT("Teting token string in input stream",
+    lex1.YYText() == "val");
+
+  LC_ASSERT("Testing token in input stream",
+    lex1.YYLex() == Lucee::LC_EQUAL);
+
+  LC_ASSERT("Testing token in input stream",
+    lex1.YYLex() == Lucee::LC_INT);
+  LC_ASSERT("Testing token value in input stream",
+    lex1.integer() == 22);
+
+  std::istringstream is2("val = \"Hello\"");
+  Lucee::KeyValTreeLexer<float> lex2(is2);
+
+  LC_ASSERT("Testing token in input stream",
+    lex2.YYLex() == Lucee::LC_ID);
+  LC_ASSERT("Teting token string in input stream",
+    lex2.YYText() == "val");
+
+  LC_ASSERT("Testing token in input stream",
+    lex2.YYLex() == Lucee::LC_EQUAL);
+
+  LC_ASSERT("Testing token in input stream",
+    lex2.YYLex() == Lucee::LC_STRING);
+  LC_ASSERT("Testing token value in input stream",
+    lex2.YYText() == "Hello");
+
+  std::istringstream is3("val = 3.14");
+  Lucee::KeyValTreeLexer<double> lex3(is3);
+
+  LC_ASSERT("Testing token in input stream",
+    lex3.YYLex() == Lucee::LC_ID);
+  LC_ASSERT("Teting token string in input stream",
+    lex3.YYText() == "val");
+
+  LC_ASSERT("Testing token in input stream",
+    lex3.YYLex() == Lucee::LC_EQUAL);
+
+  LC_ASSERT("Testing token in input stream",
+    lex3.YYLex() == Lucee::LC_REAL);
+  LC_ASSERT("Testing token value in input stream",
+    lex3.real() == 3.14);
+
+  std::istringstream is4("val = [1, 2, 3]");
+  Lucee::KeyValTreeLexer<double> lex4(is4);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_ID);
+  LC_ASSERT("Teting token string in input stream",
+    lex4.YYText() == "val");
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_EQUAL);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_LEFT_BOX);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_INT);
+  LC_ASSERT("Testing token value in input stream",
+    lex4.integer() == 1);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_COMMA);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_INT);
+  LC_ASSERT("Testing token value in input stream",
+    lex4.integer() == 2);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_COMMA);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_INT);
+  LC_ASSERT("Testing token value in input stream",
+    lex4.integer() == 3);
+
+  LC_ASSERT("Testing token in input stream",
+    lex4.YYLex() == Lucee::LC_RIGHT_BOX);
+}
+
 int
 main(void)
 {
   LC_BEGIN_TESTS("lckeyvaltreelexer");
   test_a();
+  test_b();
   LC_END_TESTS;
 }
