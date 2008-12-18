9caac2baad4d94ca31470d330a77f25b37e4c7d1
Ammar <a.hakim777@gmail.com>
2008-12-18 18:56:13 +0000

    Adding loggers. Not tested and will not compile either.

---
+++ lib/lclogger.h
@@ -0,0 +1,164 @@
+/**
+ * @file	lclogger.h
+ *
+ * @brief	Class for loggers.
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim. Released under Eclipse
+ * Licence version 1.0.
+ */
+
+#ifndef LC_LOGGER_H
+#define LC_LOGGER_H
+
+// lib includes
+#include <lcexcept.h>
+#include <lcloggerbase.h>
+
+namespace Lucee
+{
+// forward declare LogStream class
+  class Lucee::LogStream;
+
+  class Logger : public Lucee::LoggerBase<Logger>
+  {
+    public:
+      friend class Lucee::LoggerBase<Logger>;
+
+      enum MsgLevels 
+      {
+        NOTSET = 0,
+        DEBUG = 10,
+        INFO = 20,
+        WARNING = 30,
+        ERROR = 40,
+        CRITICAL = 50,
+        DISABLED = 1000
+      };
+
+      // map type to map logger level to its string representation
+      typedef std::map<unsigned, std::string, std::less<unsigned> > LevelMap_t;
+      typedef std::pair<unsigned, std::string> LevelPair_t;
+
+      typedef std::map<std::string, MsgLevels, std::less<std::string> > StringMap_t;
+      typedef std::pair<std::string, MsgLevels>  StringPair_t;
+
+/**
+ * Creates a new logger with given name. The verbosity is set
+ * NOTSET. However, by default no handlers are installed so no
+ * messages will be logged.
+ */
+      Logger(const std::string &name);
+
+/**
+ * Delete logger and all log record handlers
+ */
+      virtual ~Logger();
+
+/**
+ * Log a debug message
+ */
+      void debug(const std::string& msg) const;
+
+/**
+ * Log a info message
+ */
+      void info(const std::string& msg) const;
+
+/**
+ * Log a warning message
+ */
+      void warning(const std::string& msg) const;
+
+/**
+ * Log a error message
+ */
+      void error(const std::string& msg) const;
+
+/**
+ * Log a critical message
+ */
+      void critical(const std::string& msg) const;
+
+/**
+ * Generic messsage logger.
+ */
+      void log(const std::string& msg, MsgLevels withLevel) const;
+
+/**
+ * Set verbosity level
+ */
+      void setLevel(MsgLevels level);
+
+/**
+ * Set verbosity level passing a string
+ */
+      void setLevel(const std::string& level);
+
+/**
+ * Get verbosity level
+ */
+      MsgLevels getLevel() const;
+
+/**
+ * Get verbosity level as a string
+ */
+      std::string getLevelStr();
+
+/**
+ * Add a new handler to the logger.
+ *
+ * @param handler Instance of class LogRecordHandler. Note that the
+ * handler is not owned by the logger and hence is never deleted.
+ */
+      void addHandler(LogRecordHandler *handler);
+
+/**
+ * Disables all logging to this logger. Logging at the original level
+ * can be resumed by calling the 'enable' method.
+ */
+      void disable();
+
+/**
+ * Re-enables logging to this logger. The verbosity level is set to
+ * the one prior to the disable call.
+ */
+      void enable();
+
+/**
+ * Return stream to log debug messages
+ */
+      LogStream getDebugStream();
+
+/**
+ * Return stream to log info messages
+ */
+      LogStream getInfoStream();
+
+/**
+ * Return stream to log warning messages
+ */
+      LogStream getWarningStream();
+
+/**
+ * Return stream to log error messages
+ */
+      LogStream getErrorStream();
+
+/**
+ * Return stream to log critical messages
+ */
+      LogStream getCriticalStream();
+
+    private:
+      Logger *_parent; // parent logger
+      std::string _name;
+      MsgLevels _level, _oldLevel;
+      LevelMap_t _levelMap;
+      StringMap_t _stringMap;
+      std::vector<LogRecordHandler*> _handlers;
+  };
+}
+
+#endif // LC_LOGGER_H
---
+++ lib/lcloggerbase.h
@@ -0,0 +1,169 @@
+/**
+ * @file	lcloggerbase.h
+ *
+ * @brief	Base class for loggers.
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim. Released under Eclipse
+ * Licence version 1.0.
+ */
+
+#ifndef LC_LOGGER_BASE_H
+#define LC_LOGGER_BASE_H
+
+// lib includes
+#include <lcexcept.h>
+
+// std includes
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace Lucee
+{
+/**
+ * Base class for loggers. This class provides a method to register
+ * and access loggers.
+ */
+  template<class T>
+  class LoggerBase
+  {
+    public:
+/**
+ * Returns a logger with a given name. 
+ * 
+ * If such a logger does not exist a new logger with that name is
+ * created and returned. Note that calling this method with the same
+ * name will always return the same logger object. Thus once loggers
+ * are created they become global objects. The logger names are case
+ * sensitive.
+ *
+ * @param name specifies the logger to get. 
+ * @return the Logger object corresponding to the name.
+ */    
+      static T* get(const std::string& name);
+
+/**
+ * Returns the default logger.
+ *
+ * @return default logger
+ */
+      static T* getDefault();
+
+/**
+ * Returns a logger with a given name. If such a logger does not exist
+ * an exception is thrown.
+ *
+ * @param name specifies the logger to get. 
+ * @return the Logger object corresponding to the name.
+ */
+      static T* getSafe(const std::string& name);
+
+/**
+ * Delete all loggers registered in the system.
+ */
+      static void cleanUp();
+
+    private:
+/** Type for map of string to loggers */
+      typedef std::map<std::string, T*, std::less<std::string> > LoggerMap_t;
+/** Type for pair of string and loggers */
+      typedef std::pair<std::string, T*> LoggerPair_t;
+
+/** Map of logger names to loggers */
+      static LoggerMap_t *loggers;
+  };
+
+  // initialize maps
+  template<class T>
+  std::map<std::string, T*, std::less<std::string> >* Lucee::LoggerBase<T>::loggers = NULL;
+
+  template<class T>
+  T* 
+  Lucee::LoggerBase<T>::get(const std::string& nm) 
+  {
+    // TODO, handle this using a const at class level.
+    static const std::string defaultLoggerName("84BEF5E7-16AB-4FFF-A33B-36DD7140AA02");
+
+    if (!loggers)
+    {
+      loggers = new LoggerMap_t();
+      // Create default logger.
+      T *l = new T(defaultLoggerName);
+      loggers->insert(LoggerPair_t(defaultLoggerName, l));
+    }          
+
+    // check if logger with given name is in map
+    typename LoggerMap_t::iterator i = loggers->find(nm);
+    if (i != loggers->end())
+    {
+      return i->second;
+    }
+
+    // it is not, so start creation process
+
+    // find location of '.'
+    size_t len = 0;
+    for (size_t i = nm.size(); i >= 0; --i, ++len)
+    {
+      if ( (nm[i] == '.') || (i == 0) )
+      {
+        // create logger with given name
+        T *l = new T(nm);
+        loggers->insert(LoggerPair_t(nm, l));
+        // set its parent 
+        if (i != 0)
+        {
+          l->_parent = Lucee::LoggerBase<T>::get(nm.substr(0, nm.size()-len));
+        }
+        return l;
+      }
+    }
+    // this return statement is never executed
+    return 0;
+  }
+
+  template<class T>
+  T* 
+  Lucee::LoggerBase<T>::getDefault()
+  {
+    // TODO, handle this using a const at class level.
+    static const std::string defaultLoggerName = "84BEF5E7-16AB-4FFF-A33B-36DD7140AA02";
+    return Lucee::LoggerBase<T>::get(defaultLoggerName);
+  }
+
+  template<class T>
+  T*
+  Lucee::LoggerBase<T>::getSafe(const std::string& nm) 
+  {
+    if (!loggers)
+    {
+      loggers = new LoggerMap_t();
+    }
+
+    typename LoggerMap_t::iterator i = loggers->find(nm);
+    if (i != loggers->end())
+    {
+      return i->second;
+    }
+    // thow exception as logger not found
+    Lucee::Except wxe;
+    ex << "Logger " << std::string(nm) << " not found";
+    throw ex;
+  }
+
+  template<class T>
+  void 
+  Lucee::LoggerBase<T>::cleanUp()
+  {
+    typename LoggerMap_t::iterator itr;
+    for (itr = loggers->begin(); itr != loggers->end(); ++itr)
+    {
+      delete itr->second;
+    }
+  }
+}
+
+#endif // LC_LOGGER_BASE_H
