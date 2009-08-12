a2778d11048d6bc1712510931936493c32333e0d
a.hakim777 <a.hakim777@localhost>
2009-08-12 05:23:48 +0000

    Added missing indexr class. Also added missing test case/

--- unit/lcarray.cxx
+++ unit/lcarray.cxx
@@ -23,12 +23,12 @@ test_1()
 
   for (unsigned i=0; i<2; ++i)
   {
-    LC_ASSERT("Checking start indices", arr1.getStart(i) == 0);
-    LC_ASSERT("Checking start indices", arr2.getStart(i) == start[i]);
+    LC_ASSERT("Checking start indices", arr1.getLower(i) == 0);
+    LC_ASSERT("Checking start indices", arr2.getLower(i) == start[i]);
 
-    LC_ASSERT("Checking end indices", arr1.getEnd(i) == (int) shape[i]);
+    LC_ASSERT("Checking end indices", arr1.getUpper(i) == (int) shape[i]);
     int end = start[i]+shape[i];
-    LC_ASSERT("Checking end indices", arr2.getEnd(i) == end);
+    LC_ASSERT("Checking end indices", arr2.getUpper(i) == end);
 
     LC_ASSERT("Checking shape", arr1.getShape(i) == shape[i]);
   }
@@ -50,12 +50,46 @@ test_1()
 }
 
 void
-test_2()
+test_1d()
+{
+  unsigned shape[1] = {10};
+  Lucee::Array<1, double> arr1(shape);
+
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    arr1(i) = 10*i;
+
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    LC_ASSERT("Testing 1D indexer", arr1(i) == 10*i);
+
+  int idx[1];
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    {
+      idx[0] = i;
+      LC_ASSERT("Testing 2D indexer", arr1(idx) == 10*i);
+    }
+}
+
+void
+test_2d()
 {
   unsigned shape[2] = {5, 10};
   Lucee::Array<2, double> arr1(shape);
 
-  arr1(2,3) = 0.0;
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    for (int j=arr1.getLower(1); j<arr1.getUpper(1); ++j)
+      arr1(i,j) = 10*i+j;
+
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    for (int j=arr1.getLower(1); j<arr1.getUpper(1); ++j)
+      LC_ASSERT("Testing 2D indexer", arr1(i,j) == 10*i+j);
+
+  int idx[2];
+  for (int i=arr1.getLower(0); i<arr1.getUpper(0); ++i)
+    for (int j=arr1.getLower(1); j<arr1.getUpper(1); ++j)
+    {
+      idx[0] = i; idx[1] = j;
+      LC_ASSERT("Testing 2D indexer", arr1(idx) == 10*i+j);
+    }
 }
 
 int
@@ -63,6 +97,7 @@ main(void)
 {
   LC_BEGIN_TESTS("lcarray");
   test_1();
-  test_2();
+  test_1d();
+  test_2d();
   LC_END_TESTS;
 }
---
+++ unit/lccolmajorindexer.cxx
@@ -0,0 +1,116 @@
+/**
+ * @file	lccolmajorindexer.cxx
+ *
+ * @brief	Unit tests for Lucee::Array class
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim.
+ */
+
+// lucee includes
+#include <LcColMajorIndexer.h>
+#include <LcTest.h>
+
+void
+test_1()
+{
+  int start[1] = {1};
+  unsigned shape[1] = {10};
+  Lucee::ColMajorIndexer<1> col(start, shape);
+
+  int count=0;
+  for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+    LC_ASSERT("Testing 1D indedxer", col.getIndex(i) == count++);
+
+  count=0;
+  int idx[1];
+  for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+  {
+    idx[0] = i;
+    LC_ASSERT("Testing 1D indedxer", col.getGenIndex(idx) == count++);
+  }
+}
+
+void
+test_2()
+{
+  int start[2] = {1, 2};
+  unsigned shape[2] = {10, 15};
+  Lucee::ColMajorIndexer<2> col(start, shape);
+
+  int count=0;
+  for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+    for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+      LC_ASSERT("Testing 2D indedxer", col.getIndex(i,j) == count++);
+
+  count=0;
+  int idx[2];
+  for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+    for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+    {
+      idx[0] = i; idx[1] = j;
+      LC_ASSERT("Testing 2D indedxer", col.getGenIndex(idx) == count++);
+    }
+}
+
+void
+test_3()
+{
+  int start[3] = {1, 2, -2};
+  unsigned shape[3] = {10, 15, 20};
+  Lucee::ColMajorIndexer<3> col(start, shape);
+
+  int count=0;
+  for (int k=col.getStart(2); k<col.getEnd(2); ++k)
+    for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+      for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+        LC_ASSERT("Testing 3D indedxer", col.getIndex(i,j,k) == count++);
+
+  count=0;
+  int idx[3];
+  for (int k=col.getStart(2); k<col.getEnd(2); ++k)
+    for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+      for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+      {
+        idx[0] = i; idx[1] = j; idx[2] = k;
+        LC_ASSERT("Testing 3D indedxer", col.getGenIndex(idx) == count++);
+      }
+}
+
+void
+test_4()
+{
+  int start[4] = {1, 2, -2, 3};
+  unsigned shape[4] = {10, 15, 20, 12};
+  Lucee::ColMajorIndexer<4> col(start, shape);
+
+  int count=0;
+  for (int l=col.getStart(3); l<col.getEnd(3); ++l)
+    for (int k=col.getStart(2); k<col.getEnd(2); ++k)
+      for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+        for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+          LC_ASSERT("Testing 4D indedxer", col.getIndex(i,j,k,l) == count++);
+
+  count=0;
+  int idx[4];
+  for (int l=col.getStart(3); l<col.getEnd(3); ++l)
+    for (int k=col.getStart(2); k<col.getEnd(2); ++k)
+      for (int j=col.getStart(1); j<col.getEnd(1); ++j)
+        for (int i=col.getStart(0); i<col.getEnd(0); ++i)
+        {
+          idx[0] = i; idx[1] = j; idx[2] = k; idx[3] = l;
+          LC_ASSERT("Testing 4D indedxer", col.getGenIndex(idx) == count++);
+        }
+}
+
+int
+main(void) 
+{
+  LC_BEGIN_TESTS("lccolmajorindexer");
+  test_1();
+  test_2();
+  test_3();
+  test_4();
+  LC_END_TESTS;
+}
