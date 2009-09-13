11e57d1d27261bccf36d978ec11c2a789e8e3965
Ammar <a.hakim777@gmail.com>
2009-09-13 01:37:49 +0000

    Completed KeyVal class. Assignment and operator= not yet tested.

--- lib/LcKeyVal.cc
+++ lib/LcKeyVal.cc
@@ -22,17 +22,36 @@ namespace Lucee
   {
   }
 
-  KeyVal::~KeyVal()
-  {
-  }
-
   KeyVal::KeyVal(const KeyVal& kv)
   {
+    copyFromSet<int>(kv);
+    copyFromSet<double>(kv);
+    copyFromSet<std::string>(kv);
+    copyFromSet<std::vector<int> >(kv);
+    copyFromSet<std::vector<double> >(kv);
+    copyFromSet<std::vector<std::string> >(kv);
   }
 
   KeyVal&
   KeyVal::operator=(const KeyVal& kv)
   {
+    if (this == &kv)
+      return *this;
+// clear each of our sets
+    Loki::Field<int>(dataStoreTypeMap).dataMap.clear();
+    Loki::Field<double>(dataStoreTypeMap).dataMap.clear();
+    Loki::Field<std::string>(dataStoreTypeMap).dataMap.clear();
+    Loki::Field<std::vector<int> >(dataStoreTypeMap).dataMap.clear();
+    Loki::Field<std::vector<double> >(dataStoreTypeMap).dataMap.clear();
+    Loki::Field<std::vector<std::string> >(dataStoreTypeMap).dataMap.clear();
+// now copy stuff over
+    copyFromSet<int>(kv);
+    copyFromSet<double>(kv);
+    copyFromSet<std::string>(kv);
+    copyFromSet<std::vector<int> >(kv);
+    copyFromSet<std::vector<double> >(kv);
+    copyFromSet<std::vector<std::string> >(kv);
+
     return *this;
   }
 }
