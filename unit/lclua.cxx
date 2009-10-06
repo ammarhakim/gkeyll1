ae87a64cb27318603d3d994f06eea93ecefc5105
a.hakim777 <a.hakim777@localhost>
2009-10-06 04:26:03 +0000

    Added file.

---
+++ unit/lclua.cxx
@@ -0,0 +1,21 @@
+/**
+ * @file	lclua.cxx
+ *
+ * @brief	Unit tests for pulling LUA into Lucee
+ *
+ * @version	$Id: lclua.cxx 162 2009-08-28 20:07:10Z a.hakim777 $
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim.
+ */
+
+// lucee includes
+#include <LcTest.h>
+#include <lua.hpp>
+
+int
+main(void)
+{
+  LC_BEGIN_TESTS("lclua");
+  
+  LC_END_TESTS;
+}
