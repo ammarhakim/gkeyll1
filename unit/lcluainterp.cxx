9a2d04538fd17e24391a719d4d3a1ef0a540e4d7
a.hakim777 <a.hakim777@localhost>
2010-01-30 22:51:52 +0000

    Trying out LUA. Does not link.

---
+++ unit/lcluainterp.cxx
@@ -0,0 +1,28 @@
+/**
+ * @file	lcluainterp.cxx
+ *
+ * @brief	Unit tests for using LUA into Lucee
+ *
+ * @version	$Id: lcluainterp.cxx 162 2009-08-28 20:07:10Z a.hakim777 $
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim.
+ */
+
+// lucee includes
+#include <LcTest.h>
+#include <lua.hpp>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int
+main(void)
+{
+  LC_BEGIN_TESTS("lcluainterp");
+  
+  lua_State *L = lua_open();
+  luaL_openlibs(L);
+
+  lua_close(L);
+
+  LC_END_TESTS;
+}
