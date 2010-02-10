0427e1f5455d8dcbf3f344ef515fd74b88177cda
Ammar <a.hakim777@gmail.com>
2010-02-10 00:13:20 +0000

    Now fields compile and seem okay. Still need to create the ghost indices.

--- lib/LcField.h
+++ lib/LcField.h
@@ -26,12 +26,12 @@ namespace Lucee
 {
 /**
  * A field represents an array that can hold multiple values per index
- * location. Fields can be indexed directly using (i,j,k,...)
- * notation, with the last index into the field components, or using
- * field iterators. Further, each field can have an extra set of
+ * location. Fields can be indexed directly using (i,j,k,..,c)
+ * notation, with the last index 'c' is into the field components, or
+ * using field iterators. Further, each field can have an extra set of
  * indices outside the main indexed region which can be used to
- * represent "ghost cells or nodes". This is useful, for example, in
- * computing finite-difference stencils on the field.
+ * represent "ghost cells or ghost nodes". This is useful, for
+ * example, in computing finite-difference stencils on the field.
  */
   template <unsigned NDIM, typename T>
   class Field : public Lucee::Array<NDIM+1, T, Lucee::RowMajorIndexer<NDIM+1> >
@@ -41,29 +41,31 @@ namespace Lucee
  * Create a new field indexing given region. This constructor creates
  * an empty set of ghost indices.
  *
- * @param idxRgn Rehion indexed by array.
+ * @param rgn Region indexed by array.
  * @param nc Number of components at each index location.
  * @param init Inital value to assigned to all components.
  */      
-      Field(const Lucee::Region<NDIM, int>& idxRgn, unsigned nc, const T& init=(T)0);
+      Field(const Lucee::Region<NDIM, int>& rgn, unsigned nc, const T& init=(T)0);
 
 /**
- * Create a new field indexing given region. This constructor creates
- * an empty set of ghost indices.
+ * Number of components per index location.
  *
- * @param idxRgn Rehion indexed by array.
- * @param lowerGhost Number of ghost indices along lower side of indexed region.
- * @param upperGhost Number of ghost indices along upper side of indexed region.
- * @param nc Number of components at each index location.
- * @param init Inital value to assigned to all components.
- */      
-      Field(const Lucee::Region<NDIM, int>& idxRgn, 
-        unsigned lowerGhost[NDIM], unsigned upperGhost[NDIM],
-        unsigned nc, const T& init=(T)0);
+ * @return number of components.
+ */
+      unsigned getNumComponents() const { return numComponents; }
+
+/**
+ * Indexed region.
+ *
+ * @return indexed region.
+ */
+      Lucee::Region<NDIM, int> getRegion() const { return rgn; }
 
     private:
 /** Number of components */
       unsigned numComponents;
+/** Region indexed by grid */
+      Lucee::Region<NDIM, int> rgn;
 /** Lower and upper ghost indices */
       unsigned lowerGhost[NDIM], upperGhost[NDIM];
   };
