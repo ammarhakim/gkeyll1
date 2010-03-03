ec53e75d18196bc0e3069d200aa04634a9e85c30
Ammar <a.hakim777@gmail.com>
2010-03-03 03:25:28 +0000

    Deleted regression dir. May want to add this back later.

---
+++ slvrs/LcSolverIfc.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file	LcSolverIfc.cpp
+ *
+ * @brief	Interface class for Lucee solvers.
+ *
+ * @version	$Id: LcSolverIfc.cpp 312 2010-03-02 18:37:24Z a.hakim777 $
+ *
+ * Copyright &copy; 2008-2010, Ammar Hakim.
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcSolverIfc.h>
+
+// std includes
+#include <string>
+
+namespace Lucee
+{
+  std::string
+  SolverIfc::getName() const
+  {
+    return name;
+  }
+
+  void
+  SolverIfc::setCurrTime(double tm) 
+  {
+    currTime = tm;
+  }
+
+  double
+  SolverIfc::getCurrTime() const 
+  {
+    return currTime;
+  }
+
+  void
+  SolverIfc::setName(const std::string& nm)
+  {
+    name = nm;
+  }
+}
