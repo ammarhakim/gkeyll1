3b6b4aa613afbfc5a62f4dcf14f2042d0ceb533e
Ammar <a.hakim777@gmail.com>
2010-03-17 03:10:13 +0000

    Error handling ...

--- regression/rte/rtehomoslab-hg-1.lua
+++ regression/rte/rtehomoslab-hg-1.lua
@@ -5,7 +5,7 @@ simulation = Solver.RteHomogeneousSlab {
    -- number of phase function coefficients (degree of anisotropy)
    L = 128,
    -- number of quadrature points in each hemisphere
-   N = 64,
+   N = 62,
    -- cosine of incident angle
    mu0 = 0.5,
    -- beam flux: downward irradiance is mu0*pi*flux
@@ -18,6 +18,8 @@ simulation = Solver.RteHomogeneousSlab {
    numModes = 1,
    -- phase function
    phaseFunction = RtePhaseFunction.HG { g = 0.89 },
+   -- irradiance moments to compute
+   irradOut = {0, 1},
    -- optical depths at which outputs are to be computed
    tauOut = {0.0, 0.5, 1.0},
 }
