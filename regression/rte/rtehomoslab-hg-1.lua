71a002dedfd449d220e9a7f0c102d8d3ea467cac
Ammar <a.hakim777@gmail.com>
2010-03-18 04:18:00 +0000

    More testing ...

--- regression/rte/rtehomoslab-hg-1.lua
+++ regression/rte/rtehomoslab-hg-1.lua
@@ -5,7 +5,7 @@ simulation = Solver.RteHomogeneousSlab {
    -- number of phase function coefficients (degree of anisotropy)
    L = 128,
    -- number of quadrature points in each hemisphere
-   N = 64,
+   N = 8,
    -- cosine of incident angle
    mu0 = 0.5,
    -- beam flux: downward irradiance is mu0*pi*flux
