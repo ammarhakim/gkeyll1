efbcc7de9d0fc9f448cd5b7a97538c086b368549
a.hakim777 <a.hakim777@localhost>
2010-05-27 04:10:43 +0000

    Start of body fitted grids

---
+++ grids/LcBodyFittedGridBase.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file	LcBodyFittedGridBase.cpp
+ *
+ * @brief	Base class for body fitted grid in arbitrary dimensions.
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2010, Ammar Hakim.
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcBodyFittedGridBase.h>
+
+namespace Lucee
+{
+  template <unsigned NDIM>
+  BodyFittedGridBase<NDIM>::BodyFittedGridBase(const Lucee::Region<NDIM, int>& globalBox,
+    const Lucee::Region<NDIM, double>& compSpace)
+    : globalBox(globalBox), compSpace(compSpace), nodalCoords(globalBox, 3)
+  {
+  }
+
+// instantiations
+  template class BodyFittedGridBase<1>;
+  template class BodyFittedGridBase<2>;
+  template class BodyFittedGridBase<3>;
+}
---
+++ grids/LcBodyFittedGridBase.h
@@ -0,0 +1,52 @@
+/**
+ * @file	LcBodyFittedGridBase.h
+ *
+ * @brief	Base class for body fitted grid in arbitrary dimensions.
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2010, Ammar Hakim.
+ */
+
+#ifndef LC_BODY_FITTED_GRID_BASE_H
+#define LC_BODY_FITTED_GRID_BASE_H
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcField.h>
+#include <LcRegion.h>
+
+namespace Lucee
+{
+/**
+ * Body-fitted grid class in arbitrary dimensions. Provides an class
+ * to represent a single-block rectangular body-fitted grid.
+ */
+  template <unsigned NDIM>
+  class BodyFittedGridBase
+  {
+    public:
+/**
+ * Create a new body-fitted grid on specified region.
+ *
+ * @param globalBox Global index region for this grid.
+ * @param compSpace Region in computation space.
+ */
+      BodyFittedGridBase(const Lucee::Region<NDIM, int>& globalBox,
+        const Lucee::Region<NDIM, double>& compSpace);
+
+    private:
+/** Region indexed by grid */
+      Lucee::Region<NDIM, int> globalBox;
+/** Region spanned by grid in computational space */
+      Lucee::Region<NDIM, double> compSpace;
+/** Coordinates of nodes in grid */
+      Lucee::Field<NDIM, double> nodalCoords;
+  };
+}
+
+#endif //  LC_BODY_FITTED_GRID_H
