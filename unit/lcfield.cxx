a334f41232dcea2563e9531d4df0e497caad2f16
a.hakim777 <a.hakim777@localhost>
2010-05-28 00:49:54 +0000

    More work on fields ...

--- unit/lcfield.cxx
+++ unit/lcfield.cxx
@@ -370,6 +370,44 @@ test_10()
       }
 }
 
+void
+test_11()
+{
+  int lower[2] = {0, 0};
+  int upper[2] = {10, 12};
+  int lg[2] = {1, 1};
+  int ug[2] = {2, 2};
+  Lucee::Region<2, int> rgn(lower, upper);
+  Lucee::Field<2, double> elcField(rgn, 3, lg, ug, 10.0);
+
+  LC_ASSERT("Testing if number of components is correct", 
+    elcField.getNumComponents() == 3);
+
+  Lucee::Region<2, int> idxRgn = elcField.getRegion();
+  for (unsigned i=0; i<2; ++i)
+  {
+    LC_ASSERT("Testing if index region is correct",
+      idxRgn.getLower(i) == rgn.getLower(i));
+    LC_ASSERT("Testing if index region is correct",
+      idxRgn.getUpper(i) == rgn.getUpper(i));
+  }
+
+  for (int i=elcField.getLower(0); i<elcField.getUpper(0); ++i)
+    for (int j=elcField.getLower(1); j<elcField.getUpper(1); ++j)
+    {
+      for (unsigned k=0; k<elcField.getNumComponents(); ++k)
+        LC_ASSERT("Testing default values", elcField(i,j,k) == 10.0);
+    }
+
+  elcField = 3.0;
+  for (int i=elcField.getLower(0); i<elcField.getUpper(0); ++i)
+    for (int j=elcField.getLower(1); j<elcField.getUpper(1); ++j)
+    {
+      for (unsigned k=0; k<elcField.getNumComponents(); ++k)
+        LC_ASSERT("Testing assigned values", elcField(i,j,k) == 3.0);
+    }
+}
+
 int
 main(void)
 {
@@ -384,5 +422,6 @@ main(void)
   test_8();
   test_9();
   test_10();
+  test_11();
   LC_END_TESTS;
 }
