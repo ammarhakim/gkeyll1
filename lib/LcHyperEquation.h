178afbd215f9c65dcd0264965f95e80f7601a8a2
Ammar <a.hakim777@gmail.com>
2010-10-18 15:24:11 +0000

    Start on hyperbolic equation interface.

---
+++ lib/LcHyperEquation.h
@@ -0,0 +1,31 @@
+/**
+ * @file	LcHyperEquation.h
+ *
+ * @brief	Interface to hyperbolic equations.
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2010, Ammar Hakim.
+ */
+#ifndef LC_HYPER_EQUATION_H
+#define LC_HYPER_EQUATION_H
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcBasicObj.h>
+
+namespace Lucee
+{
+/**
+ * Represents a hyperbolic equation.
+ */
+  class HyperEquation : public Lucee::BasicObj
+  {
+  };
+}
+
+#endif //  LC_HYPER_EQUATION_H
