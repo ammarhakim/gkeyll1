8d3fb4efb1a3f1e494494122e8cfa8b8d213b2fa
a.hakim777 <a.hakim777@localhost>
2011-01-03 04:15:17 +0000

    Adding new unit test for curl calculation

---
+++ unit/lccurleval.cxx
@@ -0,0 +1,26 @@
+/**
+ * @file	lccurleval.cxx
+ *
+ * @brief	Unit tests for Lucee::CurlEval class
+ *
+ * @version	$Id$
+ *
+ * Copyright &copy; 2008-2009, Ammar Hakim.
+ */
+
+// lucee includes
+#include <LcCurlEval.h>
+#include <LcTest.h>
+
+void
+test_1()
+{
+}
+
+int
+main(void)
+{
+  LC_BEGIN_TESTS("lccurleval");
+  test_1();
+  LC_END_TESTS;
+}
