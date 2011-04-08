b1d57715cb542b23540fbca7658b0011a3ca1263
Ammar <a.hakim777@gmail.com>
2011-04-07 18:39:10 -0600

    More work on geometry

--- grids/LcUnstructGridElems.h
+++ grids/LcUnstructGridElems.h
@@ -23,7 +23,7 @@ namespace Lucee
   template <typename REAL> class UnstructGrid;
 
 /**
- * A class representig a vertex.
+ * A class representing a vertex.
  */
   template <typename REAL>
   class VertexElem
@@ -47,7 +47,10 @@ namespace Lucee
 /**
  * Increment vertex location by one.
  */
-      void incr() const;
+      void incr() const
+      {
+        currLoc += 3; // 3 as (x,y,z) coordinates are stored
+      }
 
 /**
  * Are we at end of iteration?
@@ -66,6 +69,53 @@ namespace Lucee
       std::vector<REAL> vcoords;
 /** Current location in vcoords array */
       mutable unsigned currLoc;
+  };
+
+/**
+ * A class representing a edge.
+ */
+  template <typename REAL>
+  class EdgeElem
+  {
+    public:
+/**
+ * Fill with edge center coordinates.
+ *
+ * @param xv On output coordinates of edge midpoint.
+ */
+      void fillWithCoordinates(REAL xv[3]) const;
+
+/**
+ * Get length of edge.
+ *
+ * @return lenght of edge.
+ */
+      REAL getMeasure() const;
+
+/**
+ * Fill with normal (unit vector) to edge. Note that this only makes
+ * sense when the edge lives in a 2D mesh.
+ *
+ * @param norm On output normal to edge.
+ */
+      void fillWithNormal(REAL norm[3]) const;
+
+/**
+ * Fill with a tangent (unit) to edge. This particular tangent is in
+ * the direction of the edge.
+ *
+ * @param tng On output tangent to edge.
+ */
+      void fillWithTangent1(REAL tng[3]) const;
+
+/**
+ * Fill with a tangent (unit) to edge. This particular tangent make
+ * sense only in 2D and lies in the perpendicular plane. Note that the
+ * relation norm = tangent1 X tangent 2 holds.
+ *
+ * @param tng On output tangent to edge.
+ */
+      void fillWithTangent2(REAL tng[3]) const;
   };
 
 /**
