22c02ed3a1b026e9734f679b715f9ac3050077c4
Ammar <a.hakim777@gmail.com>
2012-06-11 15:43:59 -0400

    Added field integrator for nodal fields. Seems to work fine.

---
+++ proto/LcIntegrateNodalField.cpp
@@ -0,0 +1,135 @@
+/**
+ * @file	LcIntegrateNodalField.cpp
+ *
+ * @brief	Updater to integrate nodal DG/CG field over complete domain
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcGlobals.h>
+#include <LcIntegrateNodalField.h>
+
+// loki includes
+#include <loki/Singleton.h>
+
+namespace Lucee
+{
+  template <> const char *IntegrateNodalField<1>::id = "IntegrateNodalField1D";
+  template <> const char *IntegrateNodalField<2>::id = "IntegrateNodalField2D";
+  template <> const char *IntegrateNodalField<3>::id = "IntegrateNodalField3D";
+
+  template <unsigned NDIM>
+  IntegrateNodalField<NDIM>::IntegrateNodalField()
+    : Lucee::UpdaterIfc()
+  {
+  }
+
+  template <unsigned NDIM>
+  void
+  IntegrateNodalField<NDIM>::readInput(Lucee::LuaTable& tbl)
+  {
+// call base class method
+    Lucee::UpdaterIfc::readInput(tbl);
+
+// get hold of element to use
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<NDIM> >("basis"))
+      nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<NDIM> >("basis");
+    else
+      throw Lucee::Except("IntegrateNodalField::readInput: Must specify element to use using 'basis'");
+
+    sharedNodes = false;
+// check if nodes are shared
+    if (tbl.hasBool("shareCommonNodes"))
+      sharedNodes = tbl.getBool("shareCommonNodes");
+  }
+
+  template <unsigned NDIM>
+  void
+  IntegrateNodalField<NDIM>::initialize()
+  {
+// call base class method
+    Lucee::UpdaterIfc::initialize();
+
+// get weights for quadrature
+    weights.resize(nodalBasis->getNumNodes());
+    nodalBasis->getWeights(weights);
+  }
+
+  template <unsigned NDIM>
+  Lucee::UpdaterStatus
+  IntegrateNodalField<NDIM>::update(double t)
+  {
+// get hold of grid
+    const Lucee::StructuredGridBase<NDIM>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<NDIM> >();
+// get input array
+    const Lucee::Field<NDIM, double>& fld = this->getInp<Lucee::Field<NDIM, double> >(0);
+// get output dynVector
+    Lucee::DynVector<double>& fldInt = this->getOut<Lucee::DynVector<double> >(0);
+
+    Lucee::ConstFieldPtr<double> fldPtr = fld.createConstPtr();
+
+    Lucee::Region<NDIM, int> localRgn = grid.getLocalRegion();
+// sequencer over region to update
+    Lucee::RowMajorSequencer<NDIM> seq(localRgn);
+
+    unsigned nlocal = nodalBasis->getNumNodes();
+    std::vector<double> localVals(nlocal);
+    int idx[NDIM];
+
+    double localInt = 0.0;
+// loop, performing integration
+    while (seq.step())
+    {
+      seq.fillWithIndex(idx);
+// set index into element basis
+      nodalBasis->setIndex(idx);
+
+// extract values at nodes in cell
+      if (sharedNodes)
+        nodalBasis->extractFromField(fld, localVals);
+      else
+      {
+        fld.setPtr(fldPtr, idx);
+// if nodes are not shared simply copy over data
+        for (unsigned k=0; k<nlocal; ++k)
+          localVals[k] = fldPtr[k];
+      }
+
+// perform quadrature
+      for (unsigned k=0; k<nlocal; ++k)
+        localInt += weights[k]*localVals[k];
+    }
+
+    double volInt = localInt;
+// get hold of comm pointer to do all parallel messaging
+    TxCommBase *comm = Loki::SingletonHolder<Lucee::Globals>
+      ::Instance().comm;
+// sum across all processors
+    comm->allreduce(1, &localInt, &volInt, TX_SUM);
+
+    std::vector<double> data(1);
+    data[0] = volInt;
+// push value into dynVector
+    fldInt.appendData(t, data);
+
+    return Lucee::UpdaterStatus();
+  }
+
+  template <unsigned NDIM>
+  void
+  IntegrateNodalField<NDIM>::declareTypes()
+  {
+    this->appendInpVarType(typeid(Lucee::Field<NDIM, double>));
+    this->appendOutVarType(typeid(Lucee::DynVector<double>));
+  }
+
+// instantiations
+  template class Lucee::IntegrateNodalField<1>;
+  template class Lucee::IntegrateNodalField<2>;
+  template class Lucee::IntegrateNodalField<3>;
+}
---
+++ proto/LcIntegrateNodalField.h
@@ -0,0 +1,81 @@
+/**
+ * @file	LcIntegrateNodalField.h
+ *
+ * @brief	Updater to integrate nodal DG/CG field over complete domain
+ */
+
+#ifndef LC_INTEGRATE_NODAL_FIELH_H
+#define LC_INTEGRATE_NODAL_FIELH_H
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcDynVector.h>
+#include <LcField.h>
+#include <LcNodalFiniteElementIfc.h>
+#include <LcUpdaterIfc.h>
+
+// std includes
+#include <vector>
+
+namespace Lucee
+{
+/**
+ * Updater to integrate nodal DG/CG field over complete domain.
+ */
+  template <unsigned NDIM>
+  class IntegrateNodalField : public Lucee::UpdaterIfc
+  {
+    public:
+/** Class id: this is used by registration system */
+      static const char *id;
+
+/** Create new modal DG solver in 1D */
+      IntegrateNodalField();
+
+/**
+ * Bootstrap method: Read input from specified table.
+ *
+ * @param tbl Table of input values.
+ */
+      virtual void readInput(Lucee::LuaTable& tbl);
+
+/**
+ * Initialize solver, i.e. setup initial conditions. At the end of
+ * this call, the solver should be ready for evolving the solution.
+ */
+      virtual void initialize();
+
+/**
+ * Advance the solution to specified time. Updaters that do not have a
+ * concept of time should ignore the time parameter.
+ *
+ * @param t Time to advance the solution to.
+ * @return Status of updater.
+ */
+      Lucee::UpdaterStatus update(double t);
+
+/**
+ * Declare the types of input and output variables accepted by this
+ * updater. This must be provided by the derived classes for the
+ * type-checking to pass. Inside the implementation of this method the
+ * derived class must make a sequence of appendInpVarType() and
+ * appendOutVarType() calls to declare the input/output data structure
+ * types.
+ */
+      void declareTypes();
+
+    private:
+/** Pointer to nodal basis functions to use */
+      Lucee::NodalFiniteElementIfc<NDIM> *nodalBasis;
+/** Flag to indicate if common nodes are shared */
+      bool sharedNodes;
+/** Weights for quadrature */
+      std::vector<double> weights;
+  };
+}
+
+#endif // LC_INTEGRATE_NODAL_FIELH_H
