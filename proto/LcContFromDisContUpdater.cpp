1a002282624e43fb04def5e3506f60e9b652c0af
Ammar <a.hakim777@gmail.com>
2012-06-12 16:46:24 -0400

    Added a new updater to project a 1D discontinous function to a continous basis set.

--- proto/LcContFromDisContUpdater.cpp
+++ proto/LcContFromDisContUpdater.cpp
@@ -1,8 +1,13 @@
 /**
- * @file	LcFemPoissonStructUpdater.cpp
+ * @file	LcContFromDisContUpdater.cpp
  *
- * @brief	Updater to solve Poisson equations with FEM scheme on a structured grid.
+ * @brief	Updater to get a continous function from a discontinous one.
  */
+
+// NOTE: This code is a copy of the FemPoissonStructUpdater, modified
+// slightly to the function projection. Hence it perhaps more ugly
+// than it needs to be, but higher order basis functions and the
+// nastiness for handling periodic BCs is already handled.
 
 // config stuff
 #ifdef HAVE_CONFIG_H
@@ -10,7 +15,7 @@
 #endif
 
 // lucee includes
-#include <LcFemPoissonStructUpdater.h>
+#include <LcContFromDisContUpdater.h>
 #include <LcGlobals.h>
 #include <LcMatrix.h>
 #include <LcRectCartGrid.h>
@@ -29,27 +34,16 @@
 
 namespace Lucee
 {
-  static const unsigned DX = 0;
-  static const unsigned DY = 1;
-  static const unsigned DZ = 2;
-  static const unsigned LO = 0;
-  static const unsigned HI = 1;
-  static const unsigned NO_BC = 0;
-  static const unsigned DIRICHLET_BC = 1;
-  static const unsigned NEUMANN_BC = 2;
-
-  template <> const char *FemPoissonStructUpdater<1>::id = "FemPoisson1D";
-  template <> const char *FemPoissonStructUpdater<2>::id = "FemPoisson2D";
-  template <> const char *FemPoissonStructUpdater<3>::id = "FemPoisson3D";
+  template <> const char *ContFromDisContUpdater<1>::id = "ContFromDisCont1D";
 
   template <unsigned NDIM>
-  FemPoissonStructUpdater<NDIM>::FemPoissonStructUpdater()
+  ContFromDisContUpdater<NDIM>::ContFromDisContUpdater()
     : Lucee::UpdaterIfc()
   {
   }
 
   template <unsigned NDIM>
-  FemPoissonStructUpdater<NDIM>::~FemPoissonStructUpdater()
+  ContFromDisContUpdater<NDIM>::~ContFromDisContUpdater()
   { // get rid of stuff
     MatDestroy(stiffMat);
     VecDestroy(globalSrc);
@@ -58,7 +52,7 @@ namespace Lucee
 
   template <unsigned NDIM>
   void
-  FemPoissonStructUpdater<NDIM>::readInput(Lucee::LuaTable& tbl)
+  ContFromDisContUpdater<NDIM>::readInput(Lucee::LuaTable& tbl)
   {
 // call base class method
     Lucee::UpdaterIfc::readInput(tbl);
@@ -68,97 +62,21 @@ namespace Lucee
       nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<NDIM> >("basis");
     else
       throw Lucee::Except(
-        "FemPoissonStructUpdater::readInput: Must specify element to use using 'basis'");
+        "ContFromDisContUpdater::readInput: Must specify element to use using 'basis'");
 
-// check if source nodes are shared
-    srcNodesShared = true;
+// check if source nodes are shared: this is not really needed and is
+// only put in for testing
+    srcNodesShared = false;
     if (tbl.hasBool("sourceNodesShared"))
       srcNodesShared = tbl.getBool("sourceNodesShared");
 
     for (unsigned i=0; i<NDIM; ++i) 
-      periodicFlgs[i] = false;
-
-// check if any direction is periodic
-    if (tbl.hasNumVec("periodicDirs"))
-    {
-      std::vector<double> pd = tbl.getNumVec("periodicDirs");
-      for (unsigned i=0; i<pd.size(); ++i)
-      {
-        if ((pd[i]<0) || (pd[i]>=NDIM))
-        {
-          throw Lucee::Except("FemPoissonStructUpdater:readInput: Incorrect 'periodicDirs'");
-        }
-        periodicFlgs[(unsigned) pd[i]] = true;
-      }
-    }
-
-// check if all directions are periodic
-    allPeriodic = true;
-    for (unsigned i=0; i<NDIM; ++i)
-      allPeriodic = allPeriodic && periodicFlgs[i];
-
-// get BCs to apply
-    if (tbl.hasTable("bcLeft"))
-    {
-      bc[DX][LO] = getBcData(tbl.getTable("bcLeft"));
-    }
-    if (tbl.hasTable("bcRight"))
-    {
-      bc[DX][HI] = getBcData(tbl.getTable("bcRight"));
-    }
-    if (NDIM>1)
-    {
-      if (tbl.hasTable("bcBottom"))
-      {
-        bc[DY][LO] = getBcData(tbl.getTable("bcBottom"));
-      }
-      if (tbl.hasTable("bcTop"))
-      {
-        bc[DY][HI] = getBcData(tbl.getTable("bcTop"));
-      }
-    }
-    if (NDIM>2)
-    {
-      if (tbl.hasTable("bcBack"))
-      {
-        bc[DZ][LO] = getBcData(tbl.getTable("bcBack"));
-      }
-      if (tbl.hasTable("bcFront"))
-      {
-        bc[DZ][HI] = getBcData(tbl.getTable("bcFront"));
-      }
-    }
-
-// some sanity checks: must either specify both BCs along a direction
-// or none. In the latter case the BC is assumed to be periodic.
-
-    for (unsigned dir=0; dir<NDIM; ++dir)
-    {
-      if (periodicFlgs[dir] == false)
-      {
-// ensure both BCs are specified
-        if (!bc[dir][0].isSet || !bc[dir][1].isSet)
-        {
-          Lucee::Except lce("FemPoissonStructUpdater:: Must specify BCs on each side");
-          throw lce;
-        }
-      }
-      else
-      {
-// ensure no BCs are specified
-        if (bc[dir][0].isSet || bc[dir][1].isSet)
-        {
-          Lucee::Except lce("FemPoissonStructUpdater:: Cannot specify BCs");
-          lce << " if a direction is periodic";
-          throw lce;
-        }
-      }
-    }
+      periodicFlgs[i] = true;
   }
 
   template <unsigned NDIM>
   void
-  FemPoissonStructUpdater<NDIM>::initialize()
+  ContFromDisContUpdater<NDIM>::initialize()
   {
 // call base class method
     Lucee::UpdaterIfc::initialize();
@@ -172,7 +90,7 @@ namespace Lucee
     std::map<int, int> periodicNodeMap;
 
 #ifdef HAVE_MPI
-    throw Lucee::Except("FemPoissonStructUpdater does not yet work in parallel!");
+    throw Lucee::Except("ContFromDisContUpdater does not yet work in parallel!");
 #else
 // Explicit initialization of stiffness matrix speeds up
 // initialization tremendously.
@@ -215,13 +133,13 @@ namespace Lucee
 // set index into element basis
       nodalBasis->setIndex(idx);
 
-// get local stiffness matrix
-      nodalBasis->getStiffnessMatrix(localStiff);
+// get local stiffness matrix: in this case it is really the mass matrix
+      nodalBasis->getMassMatrix(localStiff);
 // construct arrays for passing into Petsc
       for (unsigned k=0; k<nlocal; ++k)
       {
         for (unsigned m=0; m<nlocal; ++m)
-          vals[nlocal*k+m] = -localStiff(k,m); // Default PetSc layout is row-major
+          vals[nlocal*k+m] = localStiff(k,m); // Default PetSc layout is row-major
       }
 
 // get local to global mapping
@@ -234,56 +152,6 @@ namespace Lucee
 
     MatAssemblyBegin(stiffMat, MAT_FINAL_ASSEMBLY);
     MatAssemblyEnd(stiffMat, MAT_FINAL_ASSEMBLY);
-
-// modify values in stiffness matrix based on Dirichlet Bcs
-    for (unsigned d=0; d<NDIM; ++d)
-    {
-      for (unsigned side=0; side<2; ++side)
-      {
-        if (bc[d][side].isSet && bc[d][side].type == DIRICHLET_BC)
-        { // we do not need to do anything for Neumann BCs
-
-// fetch number of nodes on face of element
-          unsigned nsl = side==0 ?
-            nodalBasis->getNumSurfLowerNodes(d) : nodalBasis->getNumSurfUpperNodes(d);
-
-// allocate space for mapping
-          std::vector<int> lgSurfMap(nsl);
-
-          double dv = bc[d][side].value;
-// create region to loop over side
-          Lucee::Region<NDIM, int> defRgnG = side==0 ?
-            globalRgn.resetBounds(d, globalRgn.getLower(d), globalRgn.getLower(d)+1) :
-            globalRgn.resetBounds(d, globalRgn.getUpper(d)-1, globalRgn.getUpper(d)) ;
-// only update if we are on the correct ranks
-          Lucee::Region<NDIM, int> defRgn = defRgnG.intersect(localRgn);
-
-// loop, modifying stiffness matrix
-          Lucee::RowMajorSequencer<NDIM> seq(defRgn);
-          while (seq.step())
-          {
-            seq.fillWithIndex(idx);
-// set index into element basis
-            nodalBasis->setIndex(idx);
-// get surface nodes -> global mapping
-            if (side == 0)
-              nodalBasis->getSurfLowerLocalToGlobal(d, lgSurfMap);
-            else
-              nodalBasis->getSurfUpperLocalToGlobal(d, lgSurfMap);
-
-// reset corresponding rows (Note that some rows may be reset more
-// than once. This should not be a problem, though might make the
-// setup phase a bit slower).
-            MatZeroRows(stiffMat, nsl, &lgSurfMap[0], 1.0);
-
-// now insert row numbers with corresponding values into map for use
-// in the update method
-            for (unsigned r=0; r<nsl; ++r)
-              rowBcValues[lgSurfMap[r]] = dv;
-          }
-        }
-      }
-    }
 
 // Begin process of modification to handle periodic BCs. The code in
 // the following two loops basically does the following. It modifies
@@ -325,12 +193,12 @@ namespace Lucee
           nodalBasis->setIndex(idx);
 
 // get stiffness matrix for modification of the terms on the lower boundary
-          nodalBasis->getStiffnessMatrix(localStiff);
+          nodalBasis->getMassMatrix(localStiff);
 // construct arrays for passing into Petsc
           for (unsigned k=0; k<nlocal; ++k)
           {
             for (unsigned m=0; m<nlocal; ++m)
-              vals[nlocal*k+m] = -localStiff(k,m); // Default PetSc layout is row-major
+              vals[nlocal*k+m] = localStiff(k,m); // Default PetSc layout is row-major
           }
 
 // Now compute correct locations in global stiffness matrix to add
@@ -503,7 +371,7 @@ namespace Lucee
 
   template <unsigned NDIM>
   Lucee::UpdaterStatus
-  FemPoissonStructUpdater<NDIM>::update(double t)
+  ContFromDisContUpdater<NDIM>::update(double t)
   {
 // get hold of grid
     const Lucee::StructuredGridBase<NDIM>& grid 
@@ -537,10 +405,6 @@ namespace Lucee
 
     double vol = grid.getComputationalSpace().getVolume();
     double intSrcVol = 0.0;
-// if all directions are periodic, we need to adjust source to ensure
-// solvability of the equations
-    if (allPeriodic)
-      intSrcVol = getFieldIntegral(src, srcNodesShared)/vol;    // PetscViewer lab;
 
 // clear out existing stuff in source vector: this is required
 // otherwise successive calls to advance() will accumulate into source
@@ -717,12 +581,12 @@ namespace Lucee
     if (reason < 0) 
     {
       status = false;
-      msgStrm << FemPoissonStructUpdater<NDIM>::id << ": KSPSolve failed!";
+      msgStrm << ContFromDisContUpdater<NDIM>::id << ": KSPSolve failed!";
       msgStrm << " Petsc reason code was " << reason << ".";
     }
     else
     {
-      msgStrm << FemPoissonStructUpdater<NDIM>::id << ": KSPSolve converged.";
+      msgStrm << ContFromDisContUpdater<NDIM>::id << ": KSPSolve converged.";
     }
     msgStrm << " Number of iterations " << itNum
             << ". Final residual norm was " << resNorm;
@@ -739,7 +603,7 @@ namespace Lucee
 
   template <unsigned NDIM>
   void
-  FemPoissonStructUpdater<NDIM>::declareTypes()
+  ContFromDisContUpdater<NDIM>::declareTypes()
   {
 // takes one input (source terms)
     this->appendInpVarType(typeid(Lucee::Field<NDIM, double>));
@@ -747,88 +611,6 @@ namespace Lucee
     this->appendOutVarType(typeid(Lucee::Field<NDIM, double>));
   }
 
-  template <unsigned NDIM>
-  typename FemPoissonStructUpdater<NDIM>::FemPoissonBcData
-  FemPoissonStructUpdater<NDIM>::getBcData(const Lucee::LuaTable& bct) const
-  {
-    FemPoissonBcData bcData;
-    if (bct.getString("T") == "D")
-      bcData.type = DIRICHLET_BC;
-    else if ((bct.getString("T") == "N"))
-      bcData.type = NEUMANN_BC;
-    else
-    {
-      Lucee::Except lce(
-        "FemPoissonStructUpdater::readInput: Must specify one of \"D\" or \"N\". ");
-      lce << "Specified \"" << bct.getString("T") << " instead";
-      throw lce;
-    }
-    bcData.value = bct.getNumber("V");
-    bcData.isSet = true;
-    
-    return bcData;
-  }
-
-  template <unsigned NDIM>
-  double
-  FemPoissonStructUpdater<NDIM>::getFieldIntegral(const Lucee::Field<NDIM, double>& fld, 
-    bool shareFlag)
-  {
-    unsigned nlocal = nodalBasis->getNumNodes();
-    std::vector<double> weights(nlocal), localFld(nlocal);
-// get hold of grid
-    const Lucee::StructuredGridBase<NDIM>& grid 
-      = this->getGrid<Lucee::StructuredGridBase<NDIM> >();
-
-// create pointers
-    Lucee::ConstFieldPtr<double> fldPtr = fld.createConstPtr();
-
-    Lucee::RowMajorSequencer<NDIM> seq(grid.getLocalRegion());
-    int idx[NDIM];
-    double fldInt = 0.0;
-// loop, accumulating integral in each cell
-    while (seq.step())
-    {
-      seq.fillWithIndex(idx);
-
-// set index into element basis
-      nodalBasis->setIndex(idx);
-
-// get quadrature weights
-      nodalBasis->getWeights(weights);
-
-      if (shareFlag)
-      {
-// extract source at each node from field
-        nodalBasis->extractFromField(fld, localFld);
-      }
-      else
-      {
-        fld.setPtr(fldPtr, idx);
-// if nodes are not shared simply copy over data
-        for (unsigned k=0; k<nlocal; ++k)
-          localFld[k] = fldPtr[k];
-      }
-
-// compute contribition from this cell
-      for (unsigned k=0; k<nlocal; ++k)
-      {
-        fldInt += weights[k]*localFld[k];      
-      }
-    }
-
-    double netFldInt = fldInt;
-// get hold of comm pointer to do all parallel messaging
-    TxCommBase *comm = Loki::SingletonHolder<Lucee::Globals>
-      ::Instance().comm;
-// sum across all processors
-    comm->allreduce(1, &fldInt, &netFldInt, TX_SUM);
-
-    return netFldInt;
-  }
-
 // instantiations
-  template class FemPoissonStructUpdater<1>;
-  template class FemPoissonStructUpdater<2>;
-  template class FemPoissonStructUpdater<3>;
+  template class ContFromDisContUpdater<1>;
 }
