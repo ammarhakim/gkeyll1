658d916b41dad906a98025e0fefd9d18fe0161d1
Ammar <a.hakim777@gmail.com>
2012-06-27 12:30:06 -0400

    Code now completed. Next to test.

--- proto/LcCopy1DTo2DNodalField.cpp
+++ proto/LcCopy1DTo2DNodalField.cpp
@@ -31,7 +31,7 @@ namespace Lucee
     if (tbl.hasObject<Lucee::NodalFiniteElementIfc<1> >("basis"))
       nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<1> >("basis");
     else
-      throw Lucee::Except("NodalFiniteElementIfc::readInput: Must specify element to use using 'basis'");
+      throw Lucee::Except("Copy1DTo2DNodalField::readInput: Must specify element to use using 'basis'");
   }
 
   void
@@ -44,6 +44,56 @@ namespace Lucee
   Lucee::UpdaterStatus
   Copy1DTo2DNodalField::update(double t)
   {
+// get hold of grid
+    const Lucee::StructuredGridBase<2>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<2> >();
+
+// get input field (1d)
+    const Lucee::Field<1, double>& fld1d = this->getInp<Lucee::Field<1, double> >(0);
+// get output field (2D)
+    Lucee::Field<2, double>& fld2d = this->getOut<Lucee::Field<2, double> >(0);
+
+    unsigned polyOrder = 1;
+    if (nodalBasis->getNumNodes() == 2)
+      polyOrder = 1;
+    else if (nodalBasis->getNumNodes() == 3)
+      polyOrder = 2;
+    else
+    {
+      Lucee::Except lce("Copy1DTo2DNodalField::update: element with nodes ");
+      lce << nodalBasis->getNumNodes() << " not supported";
+      throw lce;
+    }
+
+// local region to update (This is the 2D region. The 1D region is
+// assumed to have the same cell layout as the X-direction of the 2D region)
+    Lucee::Region<2, int> localRgn = grid.getLocalRegion();
+
+    Lucee::FieldPtr<double> fld2dPtr = fld2d.createPtr();
+    std::vector<double> data1d(nodalBasis->getNumNodes());
+// loop over all X-direction cells
+    for (int i=localRgn.getLower(0)-1; i<localRgn.getUpper(0); ++i)
+    {
+      nodalBasis->setIndex(i);
+// extract data from current cell
+      nodalBasis->extractFromField(fld1d, data1d);
+
+// copy this into alll Y-direction cells
+      for (int j=localRgn.getLower(1); j<localRgn.getUpper(1)+1; ++j)
+      {
+        fld2d.setPtr(fld2dPtr, i, j);
+// copy data based on polynomial order
+        if (polyOrder == 1)
+          fld2dPtr[0] = data1d[0];
+        else
+        {
+          fld2dPtr[0] = data1d[0];
+          fld2dPtr[1] = data1d[1];
+          fld2dPtr[2] = data1d[0];
+        }
+      }
+    }
+
     return Lucee::UpdaterStatus();
   }
 
