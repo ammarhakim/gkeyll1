8afc9507f0d0732e1437aa621577788560854502
Ammar <a.hakim777@gmail.com>
2012-07-01 22:51:55 -0400

    More example of using luabind.

--- unit/lcluabind.cxx
+++ unit/lcluabind.cxx
@@ -10,11 +10,13 @@
 #include <LcLuaTable.h>
 
 // luabind
+#include <luabind/iterator_policy.hpp>
 #include <luabind/luabind.hpp>
 
 // std includes
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 void greet()
 {
@@ -37,6 +39,25 @@ class LcTest
     }
 };
 
+struct Particle
+{
+    double x, y, z;
+    double vx, vy, vz;
+};
+
+// particle list
+struct ParticleList
+{
+    std::vector<Particle> p;
+
+    ParticleList(unsigned np)
+      : p(np)
+    {}
+    
+    unsigned numPtcls() const
+    { return p.size(); }
+};
+
 int
 init(lua_State *L)
 {
@@ -51,7 +72,21 @@ init(lua_State *L)
 // class LcTest
       class_<LcTest>("LcTest")
       .def(constructor<>())
-      .def("square", &LcTest::square)
+      .def("square", &LcTest::square),
+// particle
+      class_<Particle>("Particle")
+      .def(constructor<>())
+      .def_readwrite("x", &Particle::x)
+      .def_readwrite("y", &Particle::y)
+      .def_readwrite("z", &Particle::z)
+      .def_readwrite("vx", &Particle::vx)
+      .def_readwrite("vy", &Particle::vy)
+      .def_readwrite("vz", &Particle::vz),
+// particle list
+      class_<ParticleList>("ParticleList")
+      .def(constructor<unsigned>())
+      .def_readwrite("p", &ParticleList::p, return_stl_iterator)
+      .def("numPtcls", &ParticleList::numPtcls)
     ];
 }
 
