f587eae0fa8f964bd12f11b28ecb2c7e6c06ac8b
Ammar Hakim <a.hakim777@gmail.com>
2012-07-18 23:43:13 -0400

    Updated notes.

--- unit/lcblitz.cxx
+++ unit/lcblitz.cxx
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <valarray>
 
 void
 test_1()
@@ -21,9 +22,53 @@ test_1()
   z = x+y;
 }
 
+void
+test_2()
+{
+  blitz::Array<int,2> A(4,4);
+  A = blitz::tensor::i * 10 + blitz::tensor::j; 
+  std::cout << "A = " << A << std::endl;
+  
+  blitz::Array<int,2>::iterator iter = A.begin(), end = A.end();
+
+  while (iter != end)
+  {
+    std::cout << iter.position() << '\t' << (*iter) << std::endl;
+    ++iter;
+  }
+}
+
+void
+test_3()
+{
+  blitz::Array<blitz::TinyVector<float, 3>, 2> A(5,5);
+  A = 0.0; // clear out contents
+  blitz::Array<float, 2> B = A.extractComponent(float(), 1, 3);
+  B = 10;
+  std::cout << A << std::endl;
+}
+
+void
+test_4()
+{
+  blitz::Array<blitz::TinyVector<float, 3>, 2> A(5,5);
+
+  blitz::Array<float, 2> B0 = A.extractComponent(float(), 0, 1);
+  blitz::Array<float, 2> B1 = A.extractComponent(float(), 1, 2);
+  blitz::Array<float, 2> B2 = A.extractComponent(float(), 2, 3);
+  B0 = 0;
+  B1 = 1;
+  B2 = 2;
+
+  std::cout << A << std::endl;
+}
+
 int
 main(int argc, char *argv[])
 {
   test_1();
+  test_2();
+  test_3();
+  test_4();
   return 0;
 }
