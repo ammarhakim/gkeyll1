419e427a7eba9b25d4279e592f96a1cd110512c4
Ammar Hakim <a.hakim777@gmail.com>
2012-10-14 22:33:59 -0400

    More work in setting up Lagrange tensor elements.

--- slvrs/LcLagrangeTensorBasisCalc.cpp
+++ slvrs/LcLagrangeTensorBasisCalc.cpp
@@ -10,15 +10,46 @@
 #endif
 
 // gkeyll includes
+#include <LcExcept.h>
 #include <LcLagrangeTensorBasisCalc.h>
+#include <LcMathLib.h>
 
 namespace Lucee
 {
-  LagrangeTensorBasisCalc(unsigned nd)
+  LagrangeTensorBasisCalc::LagrangeTensorBasisCalc(unsigned nd)
     : ndim(nd)
   {
-// allocate space to store number of basis in each direction
     for (unsigned n=0; n<ndim; ++n)
-      numBasis.push_back(0);
+      numNodes.push_back(0);
+  }
+
+  void
+  LagrangeTensorBasisCalc::calc(Node_t type, const std::vector<unsigned> nn)
+  {
+    if (nn.size() != ndim)
+    {
+      Lucee::Except lce("LagrangeTensorBasisCalc::calc: Must specify number of nodes in each direction.");
+      lce << " Only " << nn.size() << " directions specified for NDIM " << ndim << std::endl;
+      throw lce;
+    }
+
+    for (unsigned i=0; i<ndim; ++i) 
+      numNodes[i] = nn[i];
+
+// allocate space to store node locations
+    nodeLocs.resize(ndim);
+    for (unsigned i=0; i<ndim; ++i)
+      nodeLocs[i].loc.resize(numNodes[i]);
+
+// compute location of nodes
+    if (type == LagrangeTensorBasisCalc::LOBATTO)
+    {
+    }
+    else if (type == LagrangeTensorBasisCalc::GAUSSIAN)
+    {
+    }
+    else if (type == LagrangeTensorBasisCalc::UNIFORM)
+    {
+    }
   }
 }
