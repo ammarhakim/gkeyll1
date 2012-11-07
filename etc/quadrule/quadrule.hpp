f146e4671f1c2c5be92cb3765ab33b921a8240dd
Ammar <a.hakim777@gmail.com>
2012-11-07 10:58:06 -0500

    Put in Lobatto node calculation for abitrary number of nodes.

---
+++ etc/quadrule/quadrule.hpp
@@ -0,0 +1,12 @@
+#ifndef QUAD_RULE_HPP
+#define QUAD_RULE_HPP
+
+// This header file is just to linkage of the code in quadrule.c
+// correct from Gkeyll (which is a C++ program).
+
+extern "C"
+{
+#include <quadrule.h>
+}
+
+#endif // QUAD_RULE_HPP
