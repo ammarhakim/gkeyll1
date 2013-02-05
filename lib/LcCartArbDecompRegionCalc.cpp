891fae924875e62802682d87cbff5267e2cdd593
Ammar <a.hakim777@gmail.com>
2013-02-05 09:30:00 -0500

    Added before rename

---
+++ lib/LcCartArbDecompRegionCalc.cpp
@@ -0,0 +1,172 @@
+/**
+ * @file	LcCartProdDecompRegionCalc.cpp
+ *
+ * @brief	Decomposition with specified cuts.
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcCartProdDecompRegionCalc.h>
+#include <LcMatrix.h>
+
+// std includes
+#include <cmath>
+
+namespace Lucee
+{
+// set constructor name
+  template <> const char *CartProdDecompRegionCalc<1>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<2>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<3>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<4>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<5>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<6>::id = "CartProd";
+  template <> const char *CartProdDecompRegionCalc<7>::id = "CartProd";
+
+  template <unsigned NDIM>
+  CartProdDecompRegionCalc<NDIM>::CartProdDecompRegionCalc()
+  {
+// set some reasonable defaults
+    for (unsigned i=0; i<NDIM; ++i)
+      cuts[i] = 1; // just decompose into a single block
+    nsub = 1; // total number of regions
+  }
+
+  template <unsigned NDIM>
+  CartProdDecompRegionCalc<NDIM>::CartProdDecompRegionCalc(const unsigned c[NDIM])
+  {
+    setCuts(c);
+  }
+
+  template <unsigned NDIM>
+  CartProdDecompRegionCalc<NDIM>::CartProdDecompRegionCalc(const int c[NDIM])
+  {
+    nsub = 1;
+    for (unsigned i=0; i<NDIM; ++i)
+    {
+      cuts[i] = c[i];
+      nsub *= cuts[i];
+    }
+  }
+
+  template <unsigned NDIM>
+  void
+  CartProdDecompRegionCalc<NDIM>::readInput(Lucee::LuaTable& tbl)
+  {
+// call base class method first
+    DecompRegionCalcIfc<NDIM>::readInput(tbl);
+// now read in number of cuts
+    if (tbl.hasNumVec("cuts"))
+    {
+      std::vector<double> dblCuts = tbl.getNumVec("cuts");
+      if (dblCuts.size() != NDIM)
+      {
+        Lucee::Except lce("CartProdDecompRegionCalc::readInput: 'cuts' table must have ");
+        lce << NDIM << " entries. Only " << dblCuts.size() << " provided";
+        throw lce;
+      }
+      unsigned myCuts[NDIM];
+      for (unsigned i=0; i<NDIM; ++i)
+        myCuts[i] = (unsigned) dblCuts[i];
+      setCuts(myCuts); // set cuts
+    }
+    else
+    {
+      throw Lucee::Except("CartProdDecompRegionCalc::readInput: Must provide 'cuts' table.");
+    }
+  }
+
+  template <unsigned NDIM>
+  void
+  CartProdDecompRegionCalc<NDIM>::decompose(unsigned nrgs, const Lucee::Region<NDIM, int>& globalRgn)
+  {
+// check if cuts match up with number of regions
+    if (nrgs != nsub)
+    {
+      Lucee::Except lce("CartProdDecompRegionCalc::decompose: Number of sub-regions from cuts (");
+      lce << nsub << ") does not match requested sub-regions (" << nrgs << ")";
+      throw lce;
+    }
+
+// struct to hold shapes along each direction
+    struct { std::vector<unsigned> shape, start; } dimShapes[NDIM];
+
+// NOTE: The basic idea is to first set shape[dim] to be the nearest
+// smaller integer number of cells. Then, the remainder cells from the
+// division are added one by one to each of the cuts. This ensures
+// that each cut differs only by a single cell in each dim.
+
+// compute shapes in each direction
+    for (unsigned dim=0; dim<NDIM; ++dim)
+    {
+      dimShapes[dim].shape.resize(cuts[dim]);
+      dimShapes[dim].start.resize(cuts[dim]);
+
+ // number of cells in each sub-region by default
+      int baseShape = globalRgn.getShape(dim)/cuts[dim];
+// set default shape of each sub-region
+      for (unsigned r=0; r<cuts[dim]; ++r)
+        dimShapes[dim].shape[r] = baseShape;
+
+// remainder cells
+      int extraCells = globalRgn.getShape(dim) % cuts[dim];
+      if (extraCells > 0)
+      {
+// redistribute extra cells to each sub-region
+        unsigned r = 0; // first region to add extra cell
+        for (unsigned e=0; e<extraCells; ++e)
+        {
+          dimShapes[dim].shape[r % cuts[dim]] += 1;
+          r++;
+        }
+      }
+
+      dimShapes[dim].start[0] = globalRgn.getLower(dim);
+// set start index of each sub-region
+      for (unsigned r=1; r<cuts[dim]; ++r)
+        dimShapes[dim].start[r] = 
+          dimShapes[dim].start[r-1] + dimShapes[dim].shape[r-1];
+    }
+
+    int idx[NDIM];
+    Lucee::Region<NDIM, int> cutsRgn(cuts);
+// add regions to decomposition
+    typename Lucee::ColMajorSequencer<NDIM> seq(cutsRgn);
+    while (seq.step())
+    { // loop over region defined by cuts
+      seq.fillWithIndex(idx);
+      int lower[NDIM], upper[NDIM];
+      for (unsigned d=0; d<NDIM; ++d)
+      {
+        lower[d] = dimShapes[d].start[idx[d]];
+        upper[d] = lower[d] + dimShapes[d].shape[idx[d]];
+      }
+      this->addRegion(Lucee::Region<NDIM, int>(lower, upper));
+    }
+  }
+
+  template <unsigned NDIM>
+  void
+  CartProdDecompRegionCalc<NDIM>::setCuts(const unsigned c[NDIM])
+  {
+    nsub = 1;
+    for (unsigned i=0; i<NDIM; ++i)
+    {
+      cuts[i] = c[i];
+      nsub *= cuts[i];
+    }
+  }
+
+// instantiations
+  template class CartProdDecompRegionCalc<1>;
+  template class CartProdDecompRegionCalc<2>;
+  template class CartProdDecompRegionCalc<3>;
+  template class CartProdDecompRegionCalc<4>;
+  template class CartProdDecompRegionCalc<5>;
+  template class CartProdDecompRegionCalc<6>;
+  template class CartProdDecompRegionCalc<7>;
+}
