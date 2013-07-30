9ad60aa90ee80991646d8dcc0ad1fc91c6d36af7
Ammar Hakim <a.hakim777@gmail.com>
2013-07-30 11:18:37 -0400

    - Added new updater to set values of nodes on domain boundary - Removed tedious comments

--- slvrs/LcEvalOnBoundaryNodesUpdater.cpp
+++ slvrs/LcEvalOnBoundaryNodesUpdater.cpp
@@ -1,5 +1,5 @@
 /**
- * @file	LcEvalOnNodesUpdater.cpp
+ * @file	LcEvalOnBoundaryNodesUpdater.cpp
  *
  * @brief	Project a function of a basis functions.
  */
@@ -10,7 +10,7 @@
 #endif
 
 // lucee includes
-#include <LcEvalOnNodesUpdater.h>
+#include <LcEvalOnBoundaryNodesUpdater.h>
 #include <LcGlobals.h>
 
 // loki includes
@@ -22,19 +22,19 @@
 namespace Lucee
 {
 
-  template <> const char *EvalOnNodesUpdater<1>::id = "EvalOnNodes1D";
-  template <> const char *EvalOnNodesUpdater<2>::id = "EvalOnNodes2D";
-  template <> const char *EvalOnNodesUpdater<3>::id = "EvalOnNodes3D";
+  template <> const char *EvalOnBoundaryNodesUpdater<1>::id = "EvalOnBoundaryNodes1D";
+  template <> const char *EvalOnBoundaryNodesUpdater<2>::id = "EvalOnBoundaryNodes2D";
+  template <> const char *EvalOnBoundaryNodesUpdater<3>::id = "EvalOnBoundaryNodes3D";
 
   template <unsigned NDIM>
-  EvalOnNodesUpdater<NDIM>::EvalOnNodesUpdater()
+  EvalOnBoundaryNodesUpdater<NDIM>::EvalOnBoundaryNodesUpdater()
     : fnRef(-1), sharedNodes(false)
   {
   }
 
   template <unsigned NDIM>
   void
-  EvalOnNodesUpdater<NDIM>::readInput(Lucee::LuaTable& tbl)
+  EvalOnBoundaryNodesUpdater<NDIM>::readInput(Lucee::LuaTable& tbl)
   {
     UpdaterIfc::readInput(tbl);
 // get function to evaluate
@@ -48,25 +48,26 @@ namespace Lucee
     if (tbl.hasObject<Lucee::NodalFiniteElementIfc<NDIM> >("basis"))
       nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<NDIM> >("basis");
     else
-      throw Lucee::Except("EvalOnNodesUpdater::readInput: Must specify element to use using 'basis'");
+      throw Lucee::Except("EvalOnBoundaryNodesUpdater::readInput: Must specify element to use using 'basis'");
   }
 
   template <unsigned NDIM>
   void
-  EvalOnNodesUpdater<NDIM>::initialize()
+  EvalOnBoundaryNodesUpdater<NDIM>::initialize()
   {
     UpdaterIfc::initialize();
   }
 
   template <unsigned NDIM>
   Lucee::UpdaterStatus
-  EvalOnNodesUpdater<NDIM>::update(double t)
+  EvalOnBoundaryNodesUpdater<NDIM>::update(double t)
   {
     const Lucee::StructuredGridBase<NDIM>& grid 
       = this->getGrid<Lucee::StructuredGridBase<NDIM> >();
     Lucee::Field<NDIM, double>& q = this->getOut<Lucee::Field<NDIM, double> >(0);
     q = 0.0;
 
+// get list of nodes exclusively owned by element
     std::vector<int> ndIds;
     if (sharedNodes)
       nodalBasis->getExclusiveNodeIndices(ndIds);
@@ -79,13 +80,15 @@ namespace Lucee
 
     unsigned numNodes = ndIds.size();
     unsigned nc = q.getNumComponents()/numNodes;
+
     std::vector<double> res(nc);
     int idx[NDIM];
+
     Lucee::Matrix<double> nodeCoords(nodalBasis->getNumNodes(), 3);
 
     Lucee::LuaState *L = Loki::SingletonHolder<Lucee::Globals>::Instance().L;
-
     Lucee::FieldPtr<double> ptr = q.createPtr();
+
     Lucee::Region<NDIM, int> localExtRgn = q.getExtRegion();
     Lucee::RowMajorSequencer<NDIM> seq(localExtRgn);
     while (seq.step())
@@ -109,14 +112,14 @@ namespace Lucee
 
   template <unsigned NDIM>
   void
-  EvalOnNodesUpdater<NDIM>::declareTypes()
+  EvalOnBoundaryNodesUpdater<NDIM>::declareTypes()
   {
     this->appendOutVarType(typeid(Lucee::Field<NDIM, double>));
   }
 
   template <unsigned NDIM>
   void
-  EvalOnNodesUpdater<NDIM>::evaluateFunction(Lucee::LuaState& L, double tm,
+  EvalOnBoundaryNodesUpdater<NDIM>::evaluateFunction(Lucee::LuaState& L, double tm,
     const Lucee::Matrix<double> nc, unsigned nn, std::vector<double>& res)
   {
 // push function object on stack
@@ -128,7 +131,7 @@ namespace Lucee
 // call function
     if (lua_pcall(L, 4, res.size(), 0) != 0)
     {
-      Lucee::Except lce("EvalOnNodesUpdater::evaluateFunction: ");
+      Lucee::Except lce("EvalOnBoundaryNodesUpdater::evaluateFunction: ");
       lce << "Problem evaluating function supplied as 'evaluate' "
           << std::endl;
       std::string err(lua_tostring(L, -1));
@@ -140,14 +143,14 @@ namespace Lucee
     for (int i=-res.size(); i<0; ++i)
     {
       if (!lua_isnumber(L, i))
-        throw Lucee::Except("EvalOnNodesUpdater::evaluateFunction: Return value not a number");
+        throw Lucee::Except("EvalOnBoundaryNodesUpdater::evaluateFunction: Return value not a number");
       res[res.size()+i] = lua_tonumber(L, i);
     }
     lua_pop(L, 1);
   }
 
 // instantiations
-  template class EvalOnNodesUpdater<1>;
-  template class EvalOnNodesUpdater<2>;
-  template class EvalOnNodesUpdater<3>;
+  template class EvalOnBoundaryNodesUpdater<1>;
+  template class EvalOnBoundaryNodesUpdater<2>;
+  template class EvalOnBoundaryNodesUpdater<3>;
 }
