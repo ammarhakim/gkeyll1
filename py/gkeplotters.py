b3a1e7165b00795ffdc2f70af6cc92046bbf7f47
Ammar <a.hakim777@gmail.com>
2013-08-16 19:46:32 -0400

    More work

--- py/gkeplotters.py
+++ py/gkeplotters.py
@@ -9,8 +9,18 @@ def plotFile(fileName, argList):
         return False
     fh = tables.openFile(fileName)
     if 'StructGridField' in fh.root:
+        # process gridded data
         data = gkedata.GkeData(fileName)
+        ndim = data.ndim
+        if argList.dg_polyorder:
+            pass # doing DG
+        elif argList.cg_polyorder:
+            pass # doing CG
+        else:
+            pass # finite-volume data
+
     elif 'DataStruct' in fh.root:
+        # process history data
         pass
 
     return True
