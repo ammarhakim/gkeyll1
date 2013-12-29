b5ecbae0b18d375ee9f0ed9fe4307ee77d23815d
Ammar <a.hakim777@gmail.com>
2013-12-29 13:56:17 -0500

    Added new updater for computing perturbed Hamiltonian

--- proto/LcLinEmGke1dHamilPertUpdater.cpp
+++ proto/LcLinEmGke1dHamilPertUpdater.cpp
@@ -27,6 +27,11 @@ namespace Lucee
     Lucee::UpdaterIfc::readInput(tbl);
     charge = tbl.getNumber("charge");
     mass = tbl.getNumber("mass");
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<2> >("basis"))
+      nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<2> >("basis");
+    else
+      throw Lucee::Except("EvalOnNodesUpdater::readInput: Must specify element to use using 'basis'");
   }
 
   void
@@ -43,8 +48,39 @@ namespace Lucee
       = this->getGrid<Lucee::StructuredGridBase<2> >();
 // get input/output fields
     const Lucee::Field<2, double>& phi = this->getInp<Lucee::Field<2, double> >(0);
-    const Lucee::Field<2, double>& Apar = this->getInp<Lucee::Field<2, double> >(1);
+    const Lucee::Field<2, double>& aPar = this->getInp<Lucee::Field<2, double> >(1);
     Lucee::Field<2, double>& hamil = this->getOut<Lucee::Field<2, double> >(0);
+
+    std::vector<int> ndIds;
+    nodalBasis->getExclusiveNodeIndices(ndIds);
+    unsigned numNodes = ndIds.size(); // number of nodes owned by each cell
+
+    int idx[2];
+    Lucee::Matrix<double> nodeCoords(nodalBasis->getNumNodes(), 3);
+
+    Lucee::ConstFieldPtr<double> phiPtr = phi.createConstPtr();
+    Lucee::ConstFieldPtr<double> aParPtr = aPar.createConstPtr();
+    Lucee::FieldPtr<double> hamilPtr = hamil.createPtr();
+
+    Lucee::Region<2, int> localExtRgn = phi.getExtRegion();
+    Lucee::RowMajorSequencer<2> seq(localExtRgn);
+    while (seq.step())
+    {
+      seq.fillWithIndex(idx);
+      phi.setPtr(phiPtr, idx);
+      aPar.setPtr(aParPtr, idx);
+      hamil.setPtr(hamilPtr, idx);
+
+      nodalBasis->setIndex(idx);
+      nodalBasis->getNodalCoordinates(nodeCoords);
+
+      for (unsigned n=0; n<numNodes; ++n)
+      {
+        unsigned nn = ndIds[n]; // local node number we are working on
+// H = -q/m*w*Apar + q*phi
+        hamilPtr[n] = -charge/mass*nodeCoords(nn,1)*aParPtr[n] + charge*phiPtr[n];
+      }
+    }
 
     return Lucee::UpdaterStatus();
   }
