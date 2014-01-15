c69ed7fd05c0b95d5a72598da498b3aa1e5fdb70
Ammar <a.hakim777@gmail.com>
2014-01-15 12:47:55 -0500

    Code for C0->C1 smoothing complete. Now to test.

--- proto/LcSmoothQuadPhiToC1Updater.cpp
+++ proto/LcSmoothQuadPhiToC1Updater.cpp
@@ -15,7 +15,19 @@
 
 namespace Lucee
 {
-  static const char *id;
+  static const char *SmoothQuadPhiToC1Updaterid = "SmoothQuadPhiToC1";
+
+  static
+  double avg(double *nv)
+  {
+    return 0.5*(nv[0]+nv[2]);
+  }
+
+  static
+  double slp(double *nv)
+  {
+    return nv[2]-nv[0];
+  }
 
   SmoothQuadPhiToC1Updater::SmoothQuadPhiToC1Updater()
     : UpdaterIfc()
@@ -76,6 +88,54 @@ namespace Lucee
   Lucee::UpdaterStatus
   SmoothQuadPhiToC1Updater::update(double t)
   {
+    const Lucee::StructuredGridBase<1>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<1> >();
+    const Lucee::Field<1, double>& phiC0 = this->getInp<Lucee::Field<1, double> >(0);
+    Lucee::Field<1, double>& phiC1 = this->getOut<Lucee::Field<1, double> >(0);
+
+    Lucee::Region<1, int> localRgn = grid.getLocalRegion();
+    Lucee::RowMajorSequencer<1> seq(localRgn);
+
+    unsigned nlocal = nodalBasis->getNumNodes();
+    std::vector<double> phiK(nlocal);
+
+    std::vector<double> grad(3*(localRgn.getVolume()+2)); // +2 for ghost cells
+
+// compute gradient of potential (loop is over extended region)
+    for (unsigned i=localRgn.getLower(0)-1; i<localRgn.getUpper(0)+1; ++i)
+    {
+      nodalBasis->setIndex(i);
+// extract potential at this location
+      nodalBasis->extractFromField(phiC0, phiK);
+// compute gradient and store in appropriate location
+      matVec(1.0, diffMatrix, phiK, 0.0, &grad[3*i]);
+    }
+
+    double alpha = 1.0/3.0; // weight factor
+    double a2 = 0.5*alpha;
+
+// smooth gradient (this is actually piece-wise linear, even though
+// three local nodes are used). Edge values are stored.
+    std::vector<double> smoothedGradEdge(localRgn.getVolume()+1); // one more edge than cells
+    for (unsigned i=localRgn.getLower(0); i<localRgn.getUpper(0)+1; ++i)
+    {
+      smoothedGradEdge[i] = 0.5*(
+        avg(&grad[3*i]) - a2*slp(&grad[3*i])
+        + avg(&grad[3*(i-1)]) + a2*slp(&grad[3*(i-1)]) );
+    }    
+
+    double cumTot = smoothedGradEdge[0]; // value at the left-most edge
+    Lucee::FieldPtr<double> ptr = phiC1.createPtr();
+
+// integrate to compute a C1 approximation to potential
+    double dx = grid.getDx(0);
+    for (unsigned i=localRgn.getLower(0); i<localRgn.getUpper(0)+1; ++i)
+    {
+      phiC1.setPtr(ptr, i);
+      ptr[0] = cumTot;
+      ptr[1] = ptr[0] + dx/8*(smoothedGradEdge[i+1] + 3*smoothedGradEdge[i]);
+      cumTot += ptr[0] + dx/2*(smoothedGradEdge[i+1] + smoothedGradEdge[i]);
+    }        
 
     return Lucee::UpdaterStatus();
   }
@@ -87,5 +147,20 @@ namespace Lucee
     this->appendInpVarType(typeid(Lucee::Field<1, double>));
 // returns one output, C1 phi
     this->appendOutVarType(typeid(Lucee::Field<1, double>));
+  }
+
+  void 
+  SmoothQuadPhiToC1Updater::matVec(double m, const Lucee::Matrix<double>& mat,
+    const std::vector<double>& vec, double v, double *out)
+  {
+    double tv;
+    unsigned rows = mat.numRows(), cols = mat.numColumns();
+    for (unsigned i=0; i<rows; ++i)
+    {
+      tv = 0.0;
+      for (unsigned j=0; j<cols; ++j)
+        tv += mat(i,j)*vec[j];
+      out[i] = m*tv + v*out[i];
+    }
   }
 }
