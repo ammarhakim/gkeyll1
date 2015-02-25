71349a557937c37716d2a0e5ec1cee7ed4df86f6
Ammar Hakim <a.hakim777@gmail.com>
2015-02-25 15:12:58 -0500

    Added new code for class

--- unit/lceigen.cxx
+++ unit/lceigen.cxx
@@ -5,10 +5,17 @@
  */
 
 // lucee includes
+#include <LcLinAlgebra.h>
 #include <LcTest.h>
 
 // eigen inlcudes
 #include <Eigen/Core>
+
+// std includes
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <vector>
 
 void
 test_1()
@@ -26,10 +33,114 @@ test_1()
   LC_ASSERT("Testing cols", m.cols() == 5);
 }
 
+class Md
+{
+  public:
+    Md(unsigned r, unsigned c)
+      : r(r), c(c), data(r*c)
+    {}
+
+    inline
+    double operator()(unsigned i, unsigned j) const
+    { return data[i+j*r]; }
+
+    inline
+    double& operator()(unsigned i, unsigned j)
+    { return data[i+j*r]; }
+
+  private:
+    unsigned r, c;
+    std::vector<double> data;
+};
+
+double
+handMM(unsigned N)
+{
+  Md a(N,N), b(N,N), c(N,N);
+
+// fill a and b with random numbers
+  for (unsigned i=0; i<N; ++i)
+    for (unsigned j=0; j<N; ++j)
+    {
+      a(i,j) = std::rand()/RAND_MAX;
+      b(i,j) = std::rand()/RAND_MAX;
+      c(i,j) = 0.0;
+    }
+
+  clock_t start_t = clock();
+// compute c
+  for (unsigned i=0; i<N; ++i)
+    for (unsigned j=0; j<N; ++j)
+      for (unsigned k=0; k<N; ++k)
+        c(i,j) += a(i,k)*b(k,j);
+  clock_t end_t = clock();
+
+  return (double) (end_t-start_t)/CLOCKS_PER_SEC;
+}
+
+double
+eigenMM(unsigned N)
+{
+  Eigen::MatrixXd a(N,N), b(N,N), c(N,N);
+
+// fill a and b with random numbers
+  for (unsigned j=0; j<N; ++j)
+    for (unsigned i=0; i<N; ++i)
+    {
+      a(i,j) = std::rand()/RAND_MAX;
+      b(i,j) = std::rand()/RAND_MAX;
+    }
+// compute c
+  clock_t start_t = clock();
+  c = a*b;
+  clock_t end_t = clock();
+
+  return (double) (end_t-start_t)/CLOCKS_PER_SEC;
+}
+
+double
+blasMM(unsigned N)
+{
+  Lucee::Matrix<double> a(N,N), b(N,N), c(N,N);
+
+// fill a and b with random numbers
+  for (unsigned j=0; j<N; ++j)
+    for (unsigned i=0; i<N; ++i)
+    {
+      a(i,j) = std::rand()/RAND_MAX;
+      b(i,j) = std::rand()/RAND_MAX;
+    }
+// compute c
+  clock_t start_t = clock();
+  Lucee::accumulate(0.0, c, 1.0, a, b);
+  clock_t end_t = clock();
+
+  return (double) (end_t-start_t)/CLOCKS_PER_SEC;
+}
+
 int
 main(int argc, char *argv[])
 {
   LC_BEGIN_TESTS("lceigen");
   test_1();
+
+  std::cout << "**** N = 500" << std::endl;
+// N = 500
+  double ht = handMM(500); // HAND WRITTEN CODE
+  std::cout << "Hand-written code took " << ht << "s" << std::endl;
+  double bt = blasMM(500); // BLAS
+  std::cout << "BLAS code took " << bt << "s" << std::endl;
+  double et = eigenMM(500); // EIGEN
+  std::cout << "Eigen code took " << et << "s" << std::endl;
+
+  std::cout << "\n**** N = 1000" << std::endl;
+// N = 1000
+  ht = handMM(1000); // HAND WRITTEN CODE
+  std::cout << "Hand-written code took " << ht << "s" << std::endl;
+  bt = blasMM(1000); // BLAS
+  std::cout << "BLAS code took " << bt << "s" << std::endl;
+  et = eigenMM(1000); // EIGEN
+  std::cout << "Eigen code took " << et << "s" << std::endl;
+
   LC_END_TESTS;
 }
--- unit/lcparsum.cxx
+++ unit/lcparsum.cxx
@@ -8,10 +8,8 @@
 #include <mpi.h>
 
 // std includes
-#include <cstdlib>
 #include <iostream>
 #include <vector>
-
 
 int
 main(int argc, char *argv[])
@@ -19,23 +17,23 @@ main(int argc, char *argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm comm = MPI_COMM_WORLD;
 
-// size of array
-  unsigned nsize = 10000;
-
 // number of PUs
   int npu;
-  MPI_Comm_size(MPI_COMM_WORLD, &npu);
+  MPI_Comm_size(comm, &npu);
 // rank of current PU
   int rnk;
   MPI_Comm_rank(comm, &rnk);
 
-// store start index and size of each sub-domain
-  std::vector<unsigned> domStartIdx(npu);
-  std::vector<unsigned> domShape(npu);
+// size of array
+  unsigned nsize = 10000;
 
 // decompose global domain into npu sub-domains
   int numVals = nsize/npu;
   int extraVals = nsize % npu;
+
+// store start index and size of each sub-domain
+  std::vector<unsigned> domStartIdx(npu);
+  std::vector<unsigned> domShape(npu);
 
 // initially, each domain is the same size
   for (unsigned i=0; i<npu; ++i)
@@ -43,17 +41,13 @@ main(int argc, char *argv[])
 // distribute remaining cells so domains are almost balanced
   for (unsigned i=0; i<extraVals; ++i)
     domShape[i] += 1;
-// set initial index for each sub-domain
-  domStartIdx[0] = 0;
-  for (unsigned i=1; i<npu; ++i)
-    domStartIdx[i] = domStartIdx[i-1]+domShape[i-1];
 
 // allocate space for field
   std::vector<double> field(domShape[rnk]);
 
 // initialize field
   for (unsigned i=0; i<domShape[rnk]; ++i)
-    field[i] = 1.0;
+    field[i] = 1.0*rnk;
 
 // compute local sum of squares
   double sqSum = 0.0;
@@ -66,7 +60,7 @@ main(int argc, char *argv[])
 
 // print out result only from rank 0
   if (rnk == 0)
-    std::cout << "Sum is " << totalSum << std::endl;
+    std::cout << "Sum is " << totalSum << std::endl;  
 
   MPI_Finalize();
   return 0;
