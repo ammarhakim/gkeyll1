0713cdd2e22e1cb63d4a6469333f2800c3f57021
Ammar <a.hakim777@gmail.com>
2015-03-11 21:13:30 -0400

    - Added a new decomposition object to create decomposition based on   existing cartesian product decomposition. - Made changes to grid to actually used the appropriate communicators. - Updated the aux-advection equation to do 4D rotations. These need to   be generalized once and for all to be dimensionally independent.

--- slvrs/LcAuxAdvectionEquation.cpp
+++ slvrs/LcAuxAdvectionEquation.cpp
@@ -20,6 +20,73 @@ namespace Lucee
   template <> const char *AuxAdvectionEquation<1>::id = "AuxAdvection1D";
   template <> const char *AuxAdvectionEquation<2>::id = "AuxAdvection2D";
   template <> const char *AuxAdvectionEquation<3>::id = "AuxAdvection3D";
+  template <> const char *AuxAdvectionEquation<4>::id = "AuxAdvection4D";
+  template <> const char *AuxAdvectionEquation<5>::id = "AuxAdvection5D";
+  template <> const char *AuxAdvectionEquation<6>::id = "AuxAdvection6D";
+
+  static void rotateVecToLocal(unsigned dir, const double inVec[4], double outVec[4])
+  {
+    if (dir == 0)
+    {
+      outVec[0] = inVec[0];
+      outVec[1] = inVec[1];
+      outVec[2] = inVec[2];
+      outVec[3] = inVec[3];
+    }
+    else if (dir == 1)
+    {
+      outVec[0] = inVec[1];
+      outVec[1] = -inVec[0];
+      outVec[2] = inVec[2];
+      outVec[3] = inVec[3];
+    }
+    else if (dir == 2)
+    {
+      outVec[0] = inVec[2];
+      outVec[1] = inVec[1];
+      outVec[2] = -inVec[0];
+      outVec[3] = inVec[3];
+    }
+    else
+    {
+      outVec[0] = inVec[3];
+      outVec[1] = inVec[1];
+      outVec[2] = inVec[2];
+      outVec[3] = -inVec[0];
+    }
+  }
+
+  static void rotateVecToGlobal(unsigned dir, const double inVec[4], double outVec[4]) 
+  {
+    if (dir == 0)
+    {
+      outVec[0] = inVec[0];
+      outVec[1] = inVec[1];
+      outVec[2] = inVec[2];
+      outVec[3] = inVec[3];
+    }
+    else if (dir == 1)
+    {
+      outVec[0] = -inVec[1];
+      outVec[1] = inVec[0];
+      outVec[2] = inVec[2];
+      outVec[3] = inVec[3];
+    }
+    else if (dir == 2)
+    {
+      outVec[0] = -inVec[2];
+      outVec[1] = inVec[1];
+      outVec[2] = inVec[0];
+      outVec[3] = inVec[3];
+    }
+    else
+    {
+      outVec[0] = -inVec[3];
+      outVec[1] = inVec[1];
+      outVec[2] = inVec[2];
+      outVec[3] = inVec[0];
+    }
+  }
 
   template <unsigned NDIM>
   AuxAdvectionEquation<NDIM>::AuxAdvectionEquation()
@@ -54,12 +121,13 @@ namespace Lucee
   AuxAdvectionEquation<NDIM>::flux(const Lucee::RectCoordSys& c, const double* q, 
     const std::vector<const double*>& auxVars, double* f)
   {
-    double uvec[3] = {0, 0, 0}, uvecNorm[3];
+    double uvec[4] = {0, 0, 0, 0}, uvecNorm[4];
     for (unsigned i=0; i<NDIM; ++i)
       uvec[i] = auxVars[0][i];
 
+    unsigned dir = c.getAlignmentDirection();
 // rotate to bring into normal-tangent space
-    c.rotateVecToLocal(uvec, uvecNorm);
+    rotateVecToLocal(dir, uvec, uvecNorm);
 // compute flux
     f[0] = q[0]*uvecNorm[0];
   }
@@ -71,16 +139,18 @@ namespace Lucee
     const std::vector<const double*>& auxVarsl, const std::vector<const double*>& auxVarsr,
     double* f)
   {
-    double uvecl[3] = {0, 0, 0}, uvecr[3] = {0, 0, 0};
-    double uvecNorml[3], uvecNormr[3];
+    double uvecl[4] = {0, 0, 0, 0}, uvecr[4] = {0, 0, 0, 0};
+    double uvecNorml[4], uvecNormr[4];
     for (unsigned i=0; i<NDIM; ++i)
     {
       uvecl[i] = auxVarsl[0][i];
       uvecr[i] = auxVarsr[0][i];
     }
+
+    unsigned dir = c.getAlignmentDirection();
 // rotate to bring into normal-tangent space
-    c.rotateVecToLocal(uvecl, uvecNorml);
-    c.rotateVecToLocal(uvecr, uvecNormr);
+    rotateVecToLocal(dir, uvecl, uvecNorml);
+    rotateVecToLocal(dir, uvecr, uvecNormr);
 
     double absMaxs = std::max(std::fabs(uvecNorml[0]), std::fabs(uvecNormr[0]));
 // use Lax-fluxes
@@ -93,4 +163,7 @@ namespace Lucee
   template class AuxAdvectionEquation<1>;
   template class AuxAdvectionEquation<2>;
   template class AuxAdvectionEquation<3>;
+  template class AuxAdvectionEquation<4>;
+  template class AuxAdvectionEquation<5>;
+  template class AuxAdvectionEquation<6>;
 }
