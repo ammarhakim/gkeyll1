4061c692c316e34be18806801f59f12f5e834287
jloveric <jloveric@localhost>
2015-05-18 11:13:27 -0600

    The unstructred grid builds with a few moab commands.

--- grids/LcUnstructuredGrid.cpp
+++ grids/LcUnstructuredGrid.cpp
@@ -5,6 +5,96 @@
  *      Author: john
  */
 
-
 #include "LcUnstructuredGrid.h"
 
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcCartProdDecompRegionCalc.h>
+#include <LcGlobals.h>
+#include <LcPointerHolder.h>
+#include <LcUnstructuredGrid.h>
+
+// txbase includes
+#include <TxCommBase.h>
+
+// loki includes
+#include <loki/Singleton.h>
+
+namespace Lucee
+{
+  template<unsigned NDIM>
+  UnstructuredGrid<NDIM>::~UnstructuredGrid()
+  {
+  }
+
+  template<unsigned NDIM>
+  void UnstructuredGrid<NDIM>::readInput(Lucee::LuaTable& tbl)
+  {
+// read number of cells in global region
+    std::string filename = tbl.getString("filename");
+
+    mb = new (std::nothrow) moab::Core;
+
+    moab::ErrorCode rval = mb->load_mesh(filename.c_str());
+
+    mb->get_entities_by_dimension(0, NDIM - 1, faces);
+    mb->get_entities_by_dimension(0, NDIM, cells);
+    mb->get_entities_by_type(0, moab::MBVERTEX, vertices);
+
+// get comm pointers
+    TxCommBase * comm = Loki::SingletonHolder<Lucee::Globals>::Instance().comm;
+    TxCommBase * momComm =
+        Loki::SingletonHolder<Lucee::Globals>::Instance().comm;
+
+// set valid communicators for grid
+    this->setComm(comm);
+    this->setMomComm(momComm);
+// set I/O flag for safe ranks
+    this->setIsSafeToWrite(true);
+// compute local region
+    //localRgn = decompRgn->getRegion(comm->getRank());
+  }
+
+  template<unsigned NDIM>
+  unsigned UnstructuredGrid<NDIM>::getNumCells(unsigned dir) const
+  {
+    return cells.size();
+  }
+
+  template<unsigned NDIM>
+  void UnstructuredGrid<NDIM>::setIndex(int i) const
+  {
+    currIdx[0] = i;
+  }
+
+  template<unsigned NDIM>
+  void UnstructuredGrid<NDIM>::appendLuaCallableMethods(Lucee::LuaFuncMap& lfm)
+  {
+    //lfm.appendFunc("filename", luaGetFilename);
+  }
+
+  template<unsigned NDIM>
+  UnstructuredGrid<NDIM>::UnstructuredGrid()
+  {
+    mb = NULL;
+  }
+
+  template<unsigned NDIM>
+  UnstructuredGrid<NDIM>&
+  UnstructuredGrid<NDIM>::operator=(const UnstructuredGrid<NDIM>& sg)
+  {
+    throw Lucee::Except("UnstructuredGrid::operator=: Not implemented");
+    return *this;
+  }
+
+// instantiations
+  template class UnstructuredGrid<1> ;
+  template class UnstructuredGrid<2> ;
+  template class UnstructuredGrid<3> ;
+  template class UnstructuredGrid<4> ;
+  template class UnstructuredGrid<5> ;
+}
--- grids/LcUnstructuredGrid.h
+++ grids/LcUnstructuredGrid.h
@@ -1,11 +1,11 @@
 /**
- * @file	LcStructuredGridBase.h
+ * @file	LcUnstructuredGrid.h
  *
- * @brief	Base class for body fitted grid in arbitrary dimensions.
+ * @brief	Base class for moab unstructred grids
  */
 
-#ifndef LC_UNSTRUCTURED_GRID_BASE_H
-#define LC_UNSTRUCTURED_GRID_BASE_H
+#ifndef LC_UNSTRUCTURED_GRID_H
+#define LC_UNSTRUCTURED_GRID_H
 
 // config stuff
 #ifdef HAVE_CONFIG_H
@@ -53,34 +53,6 @@ namespace Lucee
  */
       unsigned getNumCells(unsigned dir) const;
 
-/**
- * Return global region indexed by grid.
- *
- * @return global region indexed by grid.
- */
-      Lucee::Region<NDIM, int> getGlobalRegion() const;
-
-/**
- * Return local region indexed by grid.
- *
- * @return  local region indexed by grid.
- */
-      Lucee::Region<NDIM, int> getLocalRegion() const;
-
-/**
- * Return region in computational space
- *
- * @return region in computational space
- */
-      Lucee::Region<NDIM, double> getComputationalSpace() const;
-
-/**
- * Return cell spacing in computational space in specifed direction.
- *
- * @param dir Direction in which spacing is needed.
- * @return spacing in computational space in direction 'dir'
- */
-      double getDx(unsigned dir) const;
 
 /**
  * Set the current cell location in grid to (i).
@@ -88,30 +60,6 @@ namespace Lucee
  * @param i Index location into grid.
  */
       void setIndex(int i) const;
-
-/**
- * Set the current cell location in grid to (i, j).
- *
- * @param i Index location into grid.
- * @param j Index location into grid.
- */
-      void setIndex(int i, int j) const;
-
-/**
- * Set the current cell location in grid to (i, j, k).
- *
- * @param i Index location into grid.
- * @param j Index location into grid.
- * @param k Index location into grid.
- */
-      void setIndex(int i, int j, int k) const;
-
-/**
- * Set the current cell location in grid to specified index.
- *
- * @param idx Index location into grid.
- */
-      void setIndex(const int idx[NDIM]) const;
 
 /**
  * Return coordinates in physical space of cell centroid.
@@ -145,7 +93,7 @@ namespace Lucee
 
 /**
  * Get the coordinate system attached to the surface perpendicular (in
- * computational space) to specified direction. The corrdinate system
+ * computational space) to specified direction. The coordinate system
  * is defined by three unit vectors, norm, tan1 and tan2. The norm
  * vector is normal to the surface and points into the cell. The
  * vectors tan1 and tan2 lie in the plane of the surface. The system
@@ -161,89 +109,12 @@ namespace Lucee
         double tan1[], double tan2[]) const = 0;
 
 /**
- * Get neigbors of target region for a given number of ghost cells on
- * each side of the target region. The neigbors also include corner
- * cells. This method returns those neighbors from which we should
- * receive data.
- *
- * @param rn Target region number
- * @param lowerExt Length of extension along lower end in each direction.
- * @param upperExt Length of extension along upper end in each direction.
- * @return list of neigbors region numbers.
- */
-      std::vector<unsigned> getRecvNeighbors(unsigned rn,
-        const int lowerExt[NDIM], const int upperExt[NDIM]) const;
-
-/**
- * Get neigbors of target region for a given number of ghost cells on
- * each side of the target region. The neigbors also include corner
- * cells. This method returns those neighbors to which we should send
- * data.
- *
- * @param rn Target region number
- * @param lowerExt Length of extension along lower end in each direction.
- * @param upperExt Length of extension along upper end in each direction.
- * @return list of neigbors region numbers.
- */
-      std::vector<unsigned> getSendNeighbors(unsigned rn,
-        const int lowerExt[NDIM], const int upperExt[NDIM]) const;
-
-/**
- * Get specified region from decomposition.
- *
- * @param rn Region number
- * @return Region to get.
- */
-      Lucee::Region<NDIM, int> getNeighborRgn(unsigned rn) const
-      { return decompRgn->getRegion(rn); }
-
-/**
- * Get rank of specified region
- *
- * @param rn Region number.
- * @return Rank of specified region.
- */
-      int getRgnRank(unsigned rn) const
-      { return decompRgn->getRank(rn); }
-
-/**
  * Method that performs registration of Lua functions.
  *
  * @param lfm Lua function map object.
  */
       static void appendLuaCallableMethods(Lucee::LuaFuncMap& lfm);
 
-/**
- * Lua callable method to get lower index of local box.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetLocalLower(lua_State *L);
-
-/**
- * Lua callable method to get upper index of local box.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetLocalUpper(lua_State *L);
-
-/**
- * Lua callable method to get lower index of global box.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetGlobalLower(lua_State *L);
-
-/**
- * Lua callable method to get upper index of global box.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetGlobalUpper(lua_State *L);
 
 /**
  * Lua callable method to get global shape
@@ -253,23 +124,6 @@ namespace Lucee
  */
       static int luaGetShape(lua_State *L);
 
-/**
- * Lua callable method to get coordinate of lower corner of domain in
- * computational space.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetLowerCoord(lua_State *L);
-
-/**
- * Lua callable method to get coordinate of upper corner of domain in
- * computational space.
- *
- * @param L Lua state to work with.
- * @return number of return values.
- */
-      static int luaGetUpperCoord(lua_State *L);
 
     protected:
 /**
@@ -309,37 +163,20 @@ namespace Lucee
  */
      UnstructuredGrid<NDIM>& operator=(const UnstructuredGrid<NDIM>& sg);
 
-/**
- * Set data required to build grid.
- *
- * @param localRgn Local index region for this grid.
- * @param globalRgn Global index region for this grid.
- * @param compSpace Region in computation space.
- */
-      void setGridData(const Lucee::Region<NDIM, int>& localRgn,
-        const Lucee::Region<NDIM, int>& globalRgn, const Lucee::Region<NDIM, double>& compSpace);
 
 /** Index into current cell */
       mutable int currIdx[NDIM];
-/** Local region indexed by grid */
-      Lucee::Region<NDIM, int> localRgn;
-/** Global region indexed by grid */
-      Lucee::Region<NDIM, int> globalRgn;
-/** Global region spanned by grid in computational space */
-      Lucee::Region<NDIM, double> compSpace;
 
     private:
-/** Decomposed region */
-      boost::shared_ptr<Lucee::DecompRegion<NDIM> > decompRgn;
-/** Periodic directions */
-      bool isPeriodic[NDIM];
 
-/**
- * Set direction as periodic.
- *
- * @param dir Direction to set periodic.
- */
-      void setPeriodicDir(unsigned dir);
+      //Not sure if these belong here or in geometry.
+      moab::Interface* mb;
+
+      moab::Range faces;
+      moab::Range cells;
+      moab::Range vertices;
+      moab::Range edges;
+
   };
 }
 
