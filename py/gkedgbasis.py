dbe653ec423ff6459fde26964d776963cc3ab596
Ammar Hakim <a.hakim777@gmail.com>
2015-05-21 20:50:30 -0700

    Updated to support piecwise quartic

--- py/gkedgbasis.py
+++ py/gkedgbasis.py
@@ -18,13 +18,21 @@ def evalSum(coeff, fields):
         res = res + coeff[i]*fields[i]
     return res
 
+def makeMesh(nInterp, Xc):
+    dx = Xc[1]-Xc[0]
+    nx = Xc.shape[0]
+    xlo = Xc[0]-0.5*dx
+    xup = Xc[-1]+0.5*dx
+    dx2 = dx/nInterp
+    return pylab.linspace(xlo+0.5*dx2, xup-0.5*dx2, nInterp*nx)
+
 def interpOnMesh1D(cMat, qIn):
     nInterp, nNodes = cMat.shape[0], cMat.shape[1]
     nx = qIn.shape[0]
-    qout = pylab.zeros((nInterp*nx,), pylab.float)
+    qout = pylab.zeros((nInterp*nx,), numpy.float)
     vList = [qIn[:,i] for i in range(nNodes)]
     for i in range(nInterp):
-        qout[i:nInterp*nx:nInterp] = evalSum(cMar[i,:], vList)
+        qout[i:nInterp*nx:nInterp] = evalSum(cMat[i,:], vList)
     return qout
 
 def intepOnMesh2D(cMat, qIn):
@@ -83,7 +91,7 @@ class GkeDgBasis:
     def project(self, c):
         r"""project(c : int)
         """
-        pass
+        return 0, 0
 
 
 class GkeDgPolyOrder0Basis(GkeDgBasis):
@@ -95,7 +103,7 @@ class GkeDgPolyOrder0Basis(GkeDgBasis):
         GkeDgBasis.__init__(self, dat, 1)
 
     def project(self, c):
-        return self._getRaw(c)
+        return self.Xc[0], self._getRaw(c)
 
 class GkeDgLobatto1DPolyOrder1Basis(GkeDgBasis):
     r"""Lobatto, polyOrder = 1 basis, in 1D
@@ -103,11 +111,11 @@ class GkeDgLobatto1DPolyOrder1Basis(GkeDgBasis):
 
     def __init__(self, dat):
         GkeDgBasis.__init__(self, dat, 2)
-        self.cMat_i2 = makematrix([0.75,0.25],[0.25,0.75])
+        self.cMat_i2 = makeMatrix([0.75,0.25],[0.25,0.75])
 
     def project(self, c):
         qn = self._getRaw(c)
-        return interpOnMesh1D(self.cMat_i2, qn)
+        return makeMesh(2, self.Xc[0]), interpOnMesh1D(self.cMat_i2, qn)
 
 class GkeDgLobatto1DPolyOrder2Basis(GkeDgBasis):
     r"""Lobatto, polyOrder = 2 basis in 1D
@@ -115,11 +123,11 @@ class GkeDgLobatto1DPolyOrder2Basis(GkeDgBasis):
 
     def __init__(self, dat):
         GkeDgBasis.__init__(self, dat, 3)
-        self.cMat_i2 = makeMatrix([.5555555555555556,.5555555555555556,-.1111111111111111],[0.0,1.0,0.0],[-.1111111111111111,.5555555555555556,.5555555555555556])
+        self.cMat_i3 = makeMatrix([.5555555555555556,.5555555555555556,-.1111111111111111],[0.0,1.0,0.0],[-.1111111111111111,.5555555555555556,.5555555555555556])
 
     def project(self, c):
         qn = self._getRaw(c)
-        return interpOnMesh1D(self.cMat_i3, qn)
+        return makeMesh(3, self.Xc[0]), interpOnMesh1D(self.cMat_i3, qn)
 
 class GkeDgLobatto1DPolyOrder3Basis(GkeDgBasis):
     r"""Lobatto, polyOrder = 3 basis in 1D
@@ -131,4 +139,16 @@ class GkeDgLobatto1DPolyOrder3Basis(GkeDgBasis):
 
     def project(self, c):
         qn = self._getRaw(c)
-        return interpOnMesh1D(self.cMat_i4, qn)
+        return makeMesh(4, self.Xc[0]), interpOnMesh1D(self.cMat_i4, qn)
+
+class GkeDgLobatto1DPolyOrder4Basis(GkeDgBasis):
+    r"""Lobatto, polyOrder = 4 basis in 1D
+    """
+
+    def __init__(self, dat):
+        GkeDgBasis.__init__(self, dat, 5)
+        self.cMat_i5 = makeMatrix([.2663999999999975,.8553363583762964,-0.177600000000006,.08546364162371375,-.02960000000000157],[-.1316000000000021,.7234924181056768,.5263999999999952,-.1746924181056689,.05639999999999855],[2.775557561562891e-17,1.110223024625157e-16,1.0,-5.551115123125783e-16,-1.110223024625157e-16],[.05640000000000095,-.1746924181056717,.5264000000000018,.7234924181056706,-.1315999999999997],[-.02959999999999938,0.0854636416237109,-.1775999999999993,.8553363583762902,.2663999999999995])
+
+    def project(self, c):
+        qn = self._getRaw(c)
+        return makeMesh(5, self.Xc[0]), interpOnMesh1D(self.cMat_i5, qn)
