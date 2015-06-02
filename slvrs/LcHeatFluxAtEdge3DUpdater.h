8d5b030f71aa9bade538c4e4f55d51003dfaf788
Eric Shi <eshi@pppl.gov>
2015-06-02 01:27:30 -0400

    Calculation of heat flux for 1D2V SOL problem

---
+++ slvrs/LcHeatFluxAtEdge3DUpdater.h
@@ -0,0 +1,95 @@
+/**
+ * @file	LcHeatFluxAtEdge3DUpdater.h
+ *
+ * @brief	Updater to compute heat flux at right-most edge in domain for the 3D SOL problem.
+ */
+
+#ifndef LC_HEAT_FLUX_AT_EDGE_3D_UPDATER_H
+#define LC_HEAT_FLUX_AT_EDGE_3D_UPDATER_H
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcDynVector.h>
+#include <LcField.h>
+#include <LcNodalFiniteElementIfc.h>
+#include <LcUpdaterIfc.h>
+
+// std includes
+#include <vector>
+
+// eigen includes
+#include <Eigen/Dense>
+
+namespace Lucee
+{
+/**
+ * Updater to solve hyperbolic equations using a nodal discontinous
+ * Galerkin scheme.
+ */
+  class HeatFluxAtEdge3DUpdater : public Lucee::UpdaterIfc
+  {
+    public:
+/** Class id: this is used by registration system */
+      static const char *id;
+
+/** Create new nodal DG solver */
+      HeatFluxAtEdge3DUpdater();
+
+/**
+ * Bootstrap method: Read input from specified table.
+ *
+ * @param tbl Table of input values.
+ */
+      virtual void readInput(Lucee::LuaTable& tbl);
+
+/**
+ * Initialize solver, i.e. setup initial conditions. At the end of
+ * this call, the solver should be ready for evolving the solution.
+ */
+      virtual void initialize();
+
+/**
+ * Advance the solution to specified time. Updaters that do not have a
+ * concept of time should ignore the time parameter.
+ *
+ * @param t Time to advance the solution to.
+ * @return Status of updater.
+ */
+      Lucee::UpdaterStatus update(double t);
+
+/**
+ * Declare the types of input and output variables accepted by this
+ * updater. This must be provided by the derived classes for the
+ * type-checking to pass. Inside the implementation of this method the
+ * derived class must make a sequence of appendInpVarType() and
+ * appendOutVarType() calls to declare the input/output data structure
+ * types.
+ */
+      void declareTypes();
+
+    private:
+/** Pointer to nodal basis functions to use */
+      Lucee::NodalFiniteElementIfc<1> *nodalBasis;
+/** Mass of species we are computing the heat flux using */
+      double ionMass;
+/** Weights for gaussian quadrature points */
+      std::vector<double> gaussWeights;
+/** 
+ * Interpolation matrix for bringing quantities from nodal locations to
+ * gaussian quadrature points.
+ */
+      Eigen::MatrixXd interpMatrix;
+/**
+ * Copy a Lucee-type matrix to an Eigen-type matrix.
+ * No checks are performed to make sure source and destination matrices are
+ * of the same size.
+ */
+      void copyLuceeToEigen(const Lucee::Matrix<double>& sourceMatrix, Eigen::MatrixXd& destinationMatrix);
+  };
+}
+
+#endif // LC_HEAT_FLUX_AT_EDGE_3D_UPDATER_H
