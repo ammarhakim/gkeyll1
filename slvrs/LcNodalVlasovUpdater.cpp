99f27ecc4f869a588a4afc0b4c54fd88429919a5
Ammar <a.hakim777@gmail.com>
2015-08-10 11:30:42 -0700

    Registration

--- slvrs/LcNodalVlasovUpdater.cpp
+++ slvrs/LcNodalVlasovUpdater.cpp
@@ -28,7 +28,7 @@ namespace Lucee
   template <> const char *NodalVlasovUpdater<1,3>::id = "NodalVlasov1X3V";
   template <> const char *NodalVlasovUpdater<2,2>::id = "NodalVlasov2X2V";
   template <> const char *NodalVlasovUpdater<2,3>::id = "NodalVlasov2X3V";
-  template <> const char *NodalVlasovUpdater<3,3>::id = "NodalVlasov3X3V";
+  //template <> const char *NodalVlasovUpdater<3,3>::id = "NodalVlasov3X3V";
 
   template <unsigned CDIM, unsigned VDIM>
   NodalVlasovUpdater<CDIM,VDIM>::NodalVlasovUpdater()
@@ -40,29 +40,39 @@ namespace Lucee
   void 
   NodalVlasovUpdater<CDIM,VDIM>::readInput(Lucee::LuaTable& tbl)
   {
+    const unsigned NDIM = CDIM+VDIM;
     Lucee::UpdaterIfc::readInput(tbl);
 
-    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<CDIM+VDIM> >("basis"))
-      nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<CDIM+VDIM> >("basis");
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<NDIM> >("phaseBasis"))
+      phaseBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<NDIM> >("phaseBasis");
     else
-      throw Lucee::Except("NodalVlasovUpdater::readInput: Must specify element to use using 'basis'");
+      throw Lucee::Except("NodalVlasovUpdater::readInput: Must specify phase-space basis using 'phaseBasis'");
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<CDIM> >("confBasis"))
+      confBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<CDIM> >("confBasis");
+    else
+      throw Lucee::Except("NodalVlasovUpdater::readInput: Must specify configuration-space basis using 'confBasis'");
 
 // directions to update
     if (tbl.hasNumVec("updateDirections"))
     {
       std::vector<double> ud = tbl.getNumVec("updateDirections");
-      for (unsigned i=0; i<std::min<unsigned>(3, ud.size()); ++i)
+      for (unsigned i=0; i<ud.size(); ++i)
       {
         unsigned d = (unsigned) ud[i];
-        if (d<3)
+        if (d<NDIM)
           updateDims.push_back(d);
         else
-          throw Lucee::Except("updateDirections must be a table with 0, 1, or 2");
+        {
+          Lucee::Except lce("updateDirections must be a table less than ");
+          lce << NDIM;
+          throw lce;
+        }
       }
     }
     else
     {
-      for (unsigned i=0; i<CDIM+VDIM; ++i)
+      for (unsigned i=0; i<NDIM; ++i)
         updateDims.push_back(i);
     }
 
@@ -80,13 +90,112 @@ namespace Lucee
   void 
   NodalVlasovUpdater<CDIM,VDIM>::initialize()
   {
+    const unsigned NDIM = CDIM+VDIM;
     Lucee::UpdaterIfc::initialize();
+
+// get hold of grid
+    const Lucee::StructuredGridBase<NDIM>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<NDIM> >();
+// local region to update
+    Lucee::Region<NDIM, int> localRgn = grid.getLocalRegion();
+
+    Lucee::RowMajorSequencer<NDIM> seq(localRgn);
+    seq.step(); // just to get to first index
+    int idx[NDIM];
+    seq.fillWithIndex(idx);
+    phaseBasis->setIndex(idx);
+    
+    unsigned nlocal = phaseBasis->getNumNodes();
+
+// get node numbers on each lower and upper edges
+    for (unsigned dir=0; dir<NDIM; ++dir)
+    {
+      lowerNodeNums[dir].nums.resize(phaseBasis->getNumSurfLowerNodes(dir));
+      phaseBasis->getSurfLowerNodeNums(dir, lowerNodeNums[dir].nums);
+
+      upperNodeNums[dir].nums.resize(phaseBasis->getNumSurfUpperNodes(dir));
+      phaseBasis->getSurfUpperNodeNums(dir, upperNodeNums[dir].nums);
+    }
+
+    Lucee::Matrix<double> massMatrix(nlocal, nlocal);
+
+    for (unsigned dir=0; dir<NDIM; ++dir)
+    {
+// get stiffness matrix
+      stiffMatrix[dir].m = Lucee::Matrix<double>(nlocal, nlocal);
+      phaseBasis->getGradStiffnessMatrix(dir, stiffMatrix[dir].m);
+
+      phaseBasis->getMassMatrix(massMatrix);
+      Lucee::solve(massMatrix, stiffMatrix[dir].m); // pre-multiply by inverse mass matrix
+
+// compute lift matrices
+      lowerLift[dir].m = Lucee::Matrix<double>(nlocal, 
+        phaseBasis->getNumSurfLowerNodes(dir));
+
+      phaseBasis->getLowerFaceMassMatrix(dir, lowerLift[dir].m);
+      phaseBasis->getMassMatrix(massMatrix);
+      Lucee::solve(massMatrix, lowerLift[dir].m);  // pre-multiply by inverse mass matrix
+
+      upperLift[dir].m = Lucee::Matrix<double>(nlocal, 
+        phaseBasis->getNumSurfUpperNodes(dir));
+
+      phaseBasis->getUpperFaceMassMatrix(dir, upperLift[dir].m);
+      phaseBasis->getMassMatrix(massMatrix);
+      Lucee::solve(massMatrix, upperLift[dir].m);  // pre-multiply by inverse mass matrix
+    }    
   }
 
   template <unsigned CDIM, unsigned VDIM>
   Lucee::UpdaterStatus 
   NodalVlasovUpdater<CDIM,VDIM>::update(double t)
   {
+    const unsigned NDIM = CDIM+VDIM;    
+    const Lucee::StructuredGridBase<NDIM>& grid
+      = this->getGrid<Lucee::StructuredGridBase<NDIM> >();
+
+// for compatibility with NodalDisContHyperUpdater I am retaining the
+// names "q" and "qNew" for the distribution function. Hence, q is the
+// distribution function at time t and qNew the distribution function
+// at t+dt (Ammar Hakim)
+    const Lucee::Field<NDIM, double>& q = this->getInp<Lucee::Field<NDIM, double> >(0);
+    const Lucee::Field<CDIM, double>& EM = this->getInp<Lucee::Field<CDIM, double> >(1);
+    Lucee::Field<NDIM, double>& qNew = this->getOut<Lucee::Field<NDIM, double> >(0);
+
+    unsigned nlocal = phaseBasis->getNumNodes();
+
+    double dt = t-this->getCurrTime();
+    double cfla = 0.0; // maximum CFL number used    
+    Lucee::Region<NDIM, int> localRgn = grid.getLocalRegion();
+
+    Lucee::ConstFieldPtr<double> qPtr = q.createConstPtr();
+    Lucee::ConstFieldPtr<double> qPtrl = q.createConstPtr();
+    Lucee::FieldPtr<double> qNewPtr = qNew.createPtr();
+    Lucee::FieldPtr<double> qNewPtrl = qNew.createPtr();
+    std::vector<double> flux(nlocal);
+    double localQ, localQl, localF;
+
+    qNew = 0.0; // use qNew to store increment initially    
+    
+    int idx[NDIM];
+    Lucee::RowMajorSequencer<NDIM> seq(localRgn);
+
+// loop to compute contribution from volume integrals
+    while (seq.step())
+    {
+      seq.fillWithIndex(idx);
+      q.setPtr(qPtr, idx);
+      qNew.setPtr(qNewPtr, idx);
+
+      for (unsigned d=0; d<updateDims.size(); ++d)
+      {
+        unsigned dir = updateDims[d]; // direction to update
+        for (unsigned n=0; n<nlocal; ++n)
+        {
+        }
+        //matVec(1.0, stiffMatrix[dir].m, 1, &flux[0], 1.0, &qNewPtr[0]); // stiffness X flux
+      }          
+    }
+    
     return Lucee::UpdaterStatus();
   }
 
@@ -94,12 +203,13 @@ namespace Lucee
   void
   NodalVlasovUpdater<CDIM,VDIM>::declareTypes()
   {
+    const unsigned NDIM = CDIM+VDIM;    
 // distribution function
-    this->appendInpVarType(typeid(Lucee::Field<CDIM+VDIM, double>));
+    this->appendInpVarType(typeid(Lucee::Field<NDIM, double>));
 // E and B field in a single field
     this->appendInpVarType(typeid(Lucee::Field<CDIM, double>));
-// returns one output
-    this->appendOutVarType(typeid(Lucee::Field<CDIM+VDIM, double>));
+// returns one output: updated distribution function
+    this->appendOutVarType(typeid(Lucee::Field<NDIM, double>));
   }
 
   template <unsigned CDIM, unsigned VDIM>
@@ -127,5 +237,5 @@ namespace Lucee
   template class NodalVlasovUpdater<1,3>;
   template class NodalVlasovUpdater<2,2>;
   template class NodalVlasovUpdater<2,3>;
-  template class NodalVlasovUpdater<3,3>;
+  //template class NodalVlasovUpdater<3,3>;
 }
