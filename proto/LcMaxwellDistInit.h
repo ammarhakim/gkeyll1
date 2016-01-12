f40fb7a7da4e05817fb8cd19b49e7881dd70c24b
Petr Cagas <pcagas@vt.edu>
2016-01-12 16:20:31 -0500

    Updater to initialize Maxwellian distribution created

---
+++ proto/LcMaxwellDistInit.h
@@ -0,0 +1,73 @@
+/**
+ * @file	LcMaxwellDistInit.h
+ *
+ * @brief	Updater for initializing the Maxwellian distribution from moments
+ */
+
+#ifndef LC_MAXWELL_DIST_INIT_H
+#define LC_MAXWELL_DIST_INIT_H
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcField.h>
+#include <LcNodalFiniteElementIfc.h>
+#include <LcUpdaterIfc.h>
+
+// std includes
+#include <vector>
+
+namespace Lucee
+{
+/**
+ * Maxwellian distribution.
+ */
+  template <unsigned NDIM>
+  class MaxwellDistInit : public Lucee::UpdaterIfc
+  {
+    public:
+/** Class id: this is used by registration system */
+      static const char *id;
+
+/**
+ * Bootstrap method: Read input from specified table.
+ *
+ * @param tbl Table of input values.
+ */
+      virtual void readInput(Lucee::LuaTable& tbl);
+
+/**
+ * Initialize solver, i.e. setup initial conditions. At the end of
+ * this call, the solver should be ready for evolving the solution.
+ */
+      virtual void initialize();
+
+/**
+ * Advance the solution to specified time. Updaters that do not have a
+ * concept of time should ignore the time parameter.
+ *
+ * @param t Time to advance the solution to.
+ * @return Status of updater.
+ */
+      Lucee::UpdaterStatus update(double t);
+
+/**
+ * Declare the types of input and output variables accepted by this
+ * updater. This must be provided by the derived classes for the
+ * type-checking to pass. Inside the implementation of this method the
+ * derived class must make a sequence of appendInpVarType() and
+ * appendOutVarType() calls to declare the input/output data structure
+ * types.
+ */
+      void declareTypes();
+
+    private:
+/** Pointer to nodal basis functions to use */
+      Lucee::NodalFiniteElementIfc<NDIM> *nodalBasis;
+  };
+}
+
+#endif // LC_MAXWELL_DIST_INIT_H
