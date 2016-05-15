31102b74eb5c15a12d25e1ebc6bade6add2de1b6
Eric Shi <eshi@pppl.gov>
2016-05-15 15:53:29 -0400

    Added two SOL classes to compute temperature by integrating in (v,mu) instead of computing moments via 5d-to-3d projections

---
+++ slvrs/LcSOLGeneralIntegralAtNodeCalc.cpp
@@ -0,0 +1,186 @@
+/**
+ * @file	LcSOLGeneralIntegralAtNodeCalc.cpp
+ *
+ * @brief	Compute an integral at each node in configuration space using a (v,mu) integration
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcSOLGeneralIntegralAtNodeCalc.h>
+
+namespace Lucee
+{
+  const char *SOLGeneralIntegralAtNodeCalc::id = "SOLGeneralIntegralAtNodeCalc";
+
+  SOLGeneralIntegralAtNodeCalc::SOLGeneralIntegralAtNodeCalc()
+  {
+  }
+
+  void
+  SOLGeneralIntegralAtNodeCalc::readInput(Lucee::LuaTable& tbl)
+  {
+    UpdaterIfc::readInput(tbl);
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<5> >("basis5d"))
+      nodalBasis5d = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<5> >("basis5d");
+    else
+      throw Lucee::Except("SOLGeneralIntegralAtNodeCalc::readInput: Must specify element to use using 'basis5d'");
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<3> >("basis3d"))
+      nodalBasis3d = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<3> >("basis3d");
+    else
+      throw Lucee::Except("SOLGeneralIntegralAtNodeCalc::readInput: Must specify element to use using 'basis3d'");
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<2> >("basis2d"))
+      nodalBasis2d = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<2> >("basis2d");
+    else
+      throw Lucee::Except("SOLGeneralIntegralAtNodeCalc::readInput: Must specify element to use using 'basis2d'");
+
+    if (tbl.hasNumber("scaleFactor"))
+      scaleFactor = tbl.getNumber("scaleFactor");
+    else scaleFactor = 1.0;
+  }
+
+  void
+  SOLGeneralIntegralAtNodeCalc::initialize()
+  {
+    UpdaterIfc::initialize();
+
+    unsigned nlocal = nodalBasis5d->getNumNodes();
+    std::vector<unsigned> zRef(nlocal), vRef(nlocal);
+
+    // Get a copy of the nodal coordinates
+    Lucee::Matrix<double> nodeCoordsLucee(nlocal, 5);
+    nodalBasis5d->getNodalCoordinates(nodeCoordsLucee);
+    Eigen::MatrixXd nodeCoords(nlocal, 5);
+    copyLuceeToEigen(nodeCoordsLucee, nodeCoords);
+
+    const Lucee::StructuredGridBase<5>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<5> >();
+
+    // Used to figure out which nodes share the same location in configuration space
+    double dxMin = grid.getDx(0);
+    for (int d = 1; d < 3; d++)
+      dxMin = std::min(dxMin, grid.getDx(d));
+
+    // Find all nodes that share the same location as node zero in configuration space
+    nodalStencil = std::vector<int>(nlocal);
+    int stencilIndex = 0;
+    for (int nodeIndex = 0; nodeIndex < nlocal; nodeIndex++)
+    {
+      if (sameConfigCoords(0, nodeIndex, dxMin, nodeCoords) == true)
+      {
+        nodalStencil[stencilIndex] = nodeIndex;
+        stencilIndex++;
+      }
+    }
+    nodalStencil.resize(stencilIndex);
+
+    // Compute matrix for v,mu integration
+    int nlocal2d = nodalBasis2d->getNumNodes();
+    Lucee::Matrix<double> tempMassMatrix(nlocal2d, nlocal2d);
+    integrationMatrix = Eigen::MatrixXd(nlocal2d, nlocal2d);
+    nodalBasis2d->getMassMatrix(tempMassMatrix);
+    copyLuceeToEigen(tempMassMatrix, integrationMatrix);
+    integrationMatrix *= grid.getDx(3)*grid.getDx(4)/(grid.getDx(0)*grid.getDx(1));
+  }
+
+  Lucee::UpdaterStatus
+  SOLGeneralIntegralAtNodeCalc::update(double t)
+  {
+    const Lucee::StructuredGridBase<5>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<5> >();
+
+    // Distribution function
+    const Lucee::Field<5, double>& distfIn = this->getInp<Lucee::Field<5, double> >(0);
+    // Field to multiply distribution function by before integrating
+    const Lucee::Field<5, double>& productIn = this->getInp<Lucee::Field<5, double> >(1);
+    // Output field
+    Lucee::Field<3, double>& integratedField = this->getOut<Lucee::Field<3, double> >(0);
+    
+    integratedField= 0.0; // clear out current contents
+
+    Lucee::Region<5, int> globalRgn = grid.getGlobalRegion();
+    Lucee::Region<5, int> localRgn = grid.getLocalRegion();
+    
+    Lucee::ConstFieldPtr<double> distfInPtr = distfIn.createConstPtr();
+    Lucee::ConstFieldPtr<double> productInPtr = productIn.createConstPtr();
+    Lucee::FieldPtr<double> integratedFieldPtr = integratedField.createPtr(); // Output pointer
+
+    unsigned nlocal5d = nodalBasis5d->getNumNodes();
+    unsigned nlocal3d = nodalBasis3d->getNumNodes();
+
+    int idx[5];
+
+    Lucee::RowMajorSequencer<5> seq(localRgn);
+    
+    Eigen::VectorXd distfReduced(nodalStencil.size());
+    Eigen::VectorXd productReduced(nodalStencil.size());
+
+    while (seq.step())
+    {
+      seq.fillWithIndex(idx);
+      integratedField.setPtr(integratedFieldPtr, idx[0], idx[1], idx[2]);
+      // Get the coordinates of cell center
+      grid.setIndex(idx);
+
+      distfIn.setPtr(distfInPtr, idx);
+      productIn.setPtr(productInPtr, idx);
+
+      // Loop over the  configuration space vertices in this cell
+      for (int configNode = 0; configNode < nlocal3d; configNode++)
+      {
+        // At this particular configuration space vertix, copy all
+        // nodes that occupy this location to a vector
+        for (int nodeIndex = 0; nodeIndex < nodalStencil.size(); nodeIndex++)
+        {
+          distfReduced(nodeIndex) = distfInPtr[nodalStencil[nodeIndex] + configNode];
+          productReduced(nodeIndex) = productInPtr[nodalStencil[nodeIndex] + configNode];
+        }
+
+        // Accumulate results of integration
+        integratedFieldPtr[configNode] += scaleFactor*distfReduced.dot(integrationMatrix*productReduced);
+      }
+    }
+   
+    return Lucee::UpdaterStatus();
+  }
+
+  void
+  SOLGeneralIntegralAtNodeCalc::declareTypes()
+  {
+    // Input: distribution function
+    this->appendInpVarType(typeid(Lucee::Field<5, double>));
+    // Input: optional field to multiply with the distribution function
+    this->appendInpVarType(typeid(Lucee::Field<5, double>));
+    // Output: 3d field containing mean energy at each node
+    this->appendOutVarType(typeid(Lucee::Field<3, double>));
+  }
+
+  void
+  SOLGeneralIntegralAtNodeCalc::copyLuceeToEigen(const Lucee::Matrix<double>& sourceMatrix,
+    Eigen::MatrixXd& destinationMatrix)
+  {
+    for (int rowIndex = 0; rowIndex < destinationMatrix.rows(); rowIndex++)
+    {
+      for (int colIndex = 0; colIndex < destinationMatrix.cols(); colIndex++)
+      {
+        destinationMatrix(rowIndex, colIndex) = sourceMatrix(rowIndex, colIndex);
+      }
+    }
+  }
+
+  bool
+  SOLGeneralIntegralAtNodeCalc::sameConfigCoords(int srcIndex, int tarIndex, double dxMin,
+    const Eigen::MatrixXd& nodeList)
+  {
+    for (int d = 0; d < 3; d++)
+      if (std::fabs(nodeList(srcIndex,d)-nodeList(tarIndex,d)) > 1e-4*dxMin) 
+        return false;
+    return true;
+  }
+}
---
+++ slvrs/LcSOLTemperatureAtNodeCalc.cpp
@@ -0,0 +1,114 @@
+/**
+ * @file	LcSOLTemperatureAtNodeCalc.cpp
+ *
+ * @brief	Simple updater to compute temperature node-by-node for 5D SOL simulations
+ */
+
+// config stuff
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+// lucee includes
+#include <LcSOLTemperatureAtNodeCalc.h>
+
+// loki includes
+#include <loki/Singleton.h>
+
+namespace Lucee
+{
+  const char *SOLTemperatureAtNodeCalc::id = "SOLTemperatureAtNodeCalc";
+
+  SOLTemperatureAtNodeCalc::SOLTemperatureAtNodeCalc()
+  {
+  }
+
+  void
+  SOLTemperatureAtNodeCalc::readInput(Lucee::LuaTable& tbl)
+  {
+    UpdaterIfc::readInput(tbl);
+
+    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<3> >("basis"))
+      nodalBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<3> >("basis");
+    else
+      throw Lucee::Except("SOLTemperatureAtNodeCalc::readInput: Must specify element to use using 'basis'");
+
+    if (tbl.hasNumber("speciesMass"))
+      speciesMass = tbl.getNumber("speciesMass");
+    else
+      throw Lucee::Except("SOLTemperatureAtNodeCalc::readInput: Must specify electron mass using 'speciesMass'");
+  }
+
+  void
+  SOLTemperatureAtNodeCalc::initialize()
+  {
+    UpdaterIfc::initialize();
+  }
+
+  Lucee::UpdaterStatus
+  SOLTemperatureAtNodeCalc::update(double t)
+  {
+    const Lucee::StructuredGridBase<3>& grid 
+      = this->getGrid<Lucee::StructuredGridBase<3> >();
+
+    // Input number density of species weighted by 2*pi/m
+    const Lucee::Field<3, double>& numDensIn = this->getInp<Lucee::Field<3, double> >(0);
+    // Input: <H*f> weighted by 2*pi/m
+    const Lucee::Field<3, double>& energyIn = this->getInp<Lucee::Field<3, double> >(1);
+    // Input: <dH/dv*f> weighted by 2*pi/m
+    const Lucee::Field<3, double>& meanVelocityIn = this->getInp<Lucee::Field<3, double> >(2);
+    // Input magnetic field profile
+    const Lucee::Field<3, double>& bFieldIn = this->getInp<Lucee::Field<3, double> >(3);
+    // Output dynvector of electron temperatures at wall
+    Lucee::Field<3, double>& temperatureOut = this->getOut<Lucee::Field<3, double> >(0);
+
+    Lucee::ConstFieldPtr<double> numDensPtr = numDensIn.createConstPtr();
+    Lucee::ConstFieldPtr<double> energyPtr = energyIn.createConstPtr();
+    Lucee::ConstFieldPtr<double> meanVelocityPtr = meanVelocityIn.createConstPtr();
+    Lucee::ConstFieldPtr<double> bFieldPtr = bFieldIn.createConstPtr();
+    
+    Lucee::FieldPtr<double> temperaturePtr = temperatureOut.createPtr();
+
+    unsigned nlocal = nodalBasis->getNumNodes();
+
+    double cellCentroid[3];
+    int idx[3];
+    Lucee::Region<3, int> localRgn = grid.getLocalRegion();
+    Lucee::RowMajorSequencer<3> seq(localRgn);
+
+    // Loop over local region
+    while (seq.step())
+    {
+      seq.fillWithIndex(idx);
+      
+      numDensIn.setPtr(numDensPtr, idx);
+      energyIn.setPtr(energyPtr, idx);
+      meanVelocityIn.setPtr(meanVelocityPtr, idx);
+      bFieldIn.setPtr(bFieldPtr, idx);
+      
+      temperatureOut.setPtr(temperaturePtr, idx);
+
+      for (int i = 0; i < nlocal; i++)
+        temperaturePtr[i] = 2.0/3.0*(energyPtr[i]/numDensPtr[i] - 
+          0.5*speciesMass*meanVelocityPtr[i]*meanVelocityPtr[i]/(numDensPtr[i]*numDensPtr[i]*numDensPtr[i]*
+            bFieldPtr[i]));
+    }
+    
+    return Lucee::UpdaterStatus();
+  }
+
+  void
+  SOLTemperatureAtNodeCalc::declareTypes()
+  {
+    // Input: number density of species weighted by 2*pi/m
+    this->appendInpVarType(typeid(Lucee::Field<3, double>));
+    // Input: <H*f> weighted by 2*pi/m
+    this->appendInpVarType(typeid(Lucee::Field<3, double>));
+    // Input: <dH/dv*f> weighted by 2*pi/m
+    this->appendInpVarType(typeid(Lucee::Field<3, double>));
+    // Input: magnetic field profile
+    this->appendInpVarType(typeid(Lucee::Field<3, double>));
+    // Output: Total temperature of species
+    this->appendOutVarType(typeid(Lucee::Field<3, double>));
+  }
+}
