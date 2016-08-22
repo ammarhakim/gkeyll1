a476c086a442364119a2034779cef012cb14ff57
James Juno <jjuno@umd.edu>
2016-08-22 12:08:16 -0400

    The getWeights routine has been expanded in the Serendipity class (with how this is calculated in ComputeSerendipityMatricesGeneral) so one can use routines like integrateNodalField with the Serendipity class. Further, the positivity algorithm is changed so as to instead compute <f> (before I was computing <n>) and rescaling the distribution function by the ratio of <f> before and after zeroing out negative cells to keep the total number of particles in a cell constant. This appears to be much better behaved.

--- slvrs/LcVlasovPositivityUpdater.cpp
+++ slvrs/LcVlasovPositivityUpdater.cpp
@@ -30,18 +30,6 @@ namespace Lucee
   template <> const char *VlasovPositivityUpdater<2,3>::id = "VlasovPositivity2X3V";
   //template <> const char *VlasovPositivityUpdater<3,3>::id = "VlasovPositivity3X3V";
 
-
-  template <unsigned CDIM, unsigned VDIM>
-  bool
-  VlasovPositivityUpdater<CDIM,VDIM>::sameConfigCoords(unsigned n, unsigned cn, double dxMin,
-    const Eigen::MatrixXd& phaseC, const Eigen::MatrixXd& confC)
-  {
-    for (unsigned dim = 0; dim<CDIM; ++dim)
-      if (! (std::fabs(phaseC(n, dim)-confC(cn, dim))<1e-4*dxMin) )
-        return false;
-    return true;
-  }
-
   template <unsigned CDIM, unsigned VDIM>
   VlasovPositivityUpdater<CDIM, VDIM>::VlasovPositivityUpdater()
     : Lucee::UpdaterIfc()
@@ -61,13 +49,6 @@ namespace Lucee
     else
       throw Lucee::Except(
         "VlasovPositivityUpdater::readInput: Must specify phase-space basis using 'phaseBasis'");
-
-    // get hold of configuration space element to use
-    if (tbl.hasObject<Lucee::NodalFiniteElementIfc<CDIM> >("confBasis"))
-      confBasis = &tbl.getObjectAsBase<Lucee::NodalFiniteElementIfc<CDIM> >("confBasis");
-    else
-      throw Lucee::Except(
-        "VlasovPositivityUpdater::readInput: Must specify configuration-space basis using 'confBasis'");
   }
 
   template <unsigned CDIM, unsigned VDIM>
@@ -85,91 +66,16 @@ namespace Lucee
     Lucee::Region<NDIM, int> localRgn = grid.getLocalRegion();
 
     // get number of nodes in configuration space and phase space
-    unsigned nlocalConf = confBasis->getNumNodes();
     unsigned nlocalPhase = phaseBasis->getNumNodes();
+    // get weights for quadrature
+    volWeights = Eigen::VectorXd::Zero(nlocalPhase);
+    std::vector<double> weights(nlocalPhase);
+    phaseBasis->getWeights(weights);
 
-    // get volume interpolation matrices for configuration space element
-    int nVolQuadConf = confBasis->getNumGaussNodes();
-    std::vector<double> tempVolWeightsConf(nVolQuadConf);
-    Lucee::Matrix<double> tempVolQuadConf(nVolQuadConf, nlocalConf);
-    Lucee::Matrix<double> tempVolCoordsConf(nVolQuadConf, CNC);
-
-    confBasis->getGaussQuadData(tempVolQuadConf, tempVolCoordsConf, tempVolWeightsConf);
-
-    Eigen::MatrixXd volQuadConf(nVolQuadConf, nlocalConf);
-    copyLuceeToEigen(tempVolQuadConf, volQuadConf);
-    Eigen::MatrixXd volCoordsConf(nVolQuadConf, (unsigned) CNC);
-    copyLuceeToEigen(tempVolCoordsConf, volCoordsConf);
-
-    volWeightsConf = Eigen::VectorXd::Zero(nlocalConf);
-    for(int i=0; i<nVolQuadConf; ++i)
-      volWeightsConf(i) = tempVolWeightsConf[i];
-
-    // get volume interpolation matrices for phase space element
-    int nVolQuadPhase = phaseBasis->getNumGaussNodes();
-    std::vector<double> volWeightsPhase(nVolQuadPhase);
-    Lucee::Matrix<double> tempVolQuadPhase(nVolQuadPhase, nlocalPhase);
-    Lucee::Matrix<double> tempVolCoordsPhase(nVolQuadPhase, (unsigned) PNC);
-
-    phaseBasis->getGaussQuadData(tempVolQuadPhase, tempVolCoordsPhase, volWeightsPhase);
-
-    Eigen::MatrixXd volQuadPhase(nVolQuadPhase, nlocalPhase);
-    copyLuceeToEigen(tempVolQuadPhase, volQuadPhase);
-    Eigen::MatrixXd volCoordsPhase(nVolQuadPhase, (unsigned) PNC);
-    copyLuceeToEigen(tempVolCoordsPhase, volCoordsPhase);
-
-    // compute mapping of phase-space quadrature nodes to configuration space
-    // quadrature nodes. The assumption here is that the node layout in phase-space
-    // and configuration space are such that each node in phase-space has
-    // exactly one node co-located with it in configuration space. No
-    // "orphan" phase-space node are allowed, and an exception is thrown
-    // if that occurs.
-    phaseConfMap.resize(volWeightsPhase.size());
-
-    double dxMin = grid.getDx(0);
-    for (unsigned d=1; d<CDIM; ++d)
-      dxMin = std::min(dxMin, grid.getDx(d));
-
-    for (unsigned n=0; n<volWeightsPhase.size(); ++n)
-    {
-      bool pcFound = false;
-      for (unsigned cn=0; cn<tempVolWeightsConf.size(); ++cn)
-        if (sameConfigCoords(n, cn, dxMin, volCoordsPhase, volCoordsConf))
-        {
-          phaseConfMap[n] = cn;
-          pcFound = true;
-          break;
-        }
-      if (!pcFound)
-      {
-        Lucee::Except lce(
-          "VlasovPositivityUpdater::initialize: No matching configuration space quadrature node for phase-space quadrature node ");
-        lce << n;
-        throw lce;
-      }
-    }
-
-    // Get configuration space mass matrix
-    Lucee::Matrix<double> tempMassMatrixConf(nlocalConf, nlocalConf);
-    confBasis->getMassMatrix(tempMassMatrixConf);
-    Eigen::MatrixXd massMatrixConf(nlocalConf, nlocalConf);
-    copyLuceeToEigen(tempMassMatrixConf, massMatrixConf);
-
-    mom0Matrix = Eigen::MatrixXd::Zero(nlocalConf, nlocalPhase);    
-    for (int i=0; i<nlocalConf; ++i)
-    {
-      for (int j=0; j<nlocalPhase; ++j)
-      {
-        double integralResultZerothMoment = 0;
-        // Compute integral of phiConf_i * phiPhase_j
-        for (int gaussIndex = 0; gaussIndex < volWeightsPhase.size(); ++gaussIndex)
-          integralResultZerothMoment
-            += volWeightsPhase[gaussIndex]*volQuadConf(phaseConfMap[gaussIndex],i)*volQuadPhase(gaussIndex,j);
-        mom0Matrix(i, j) = integralResultZerothMoment;
-      }
-    }
-    // Multiply matrices by inverse of mass matrix
-    mom0Matrix = massMatrixConf.inverse()*mom0Matrix;
+    double vol = grid.getVolume();
+    // normalize weights as we are computing averages and not integrals over cell
+    for (unsigned i=0; i<nlocalPhase; ++i)
+      volWeights(i) = weights[i]/vol;
   }
   template <unsigned CDIM, unsigned VDIM>
   Lucee::UpdaterStatus
@@ -183,7 +89,6 @@ namespace Lucee
     // get output field (CDIM + VDIM) 
     Lucee::Field<NDIM, double>& distfOut = this->getOut<Lucee::Field<NDIM, double> >(0);
 
-    unsigned nlocalConf = confBasis->getNumNodes();
     unsigned nlocalPhase = phaseBasis->getNumNodes();
     Lucee::FieldPtr<double> distfPtr = distfOut.createPtr();
 
@@ -194,7 +99,6 @@ namespace Lucee
 
     // Used in the sequencer
     Eigen::VectorXd distfVector(nlocalPhase);
-    Eigen::VectorXd tempNumDensity(nlocalConf);
 
     while (seq.step())
     {
@@ -206,12 +110,11 @@ namespace Lucee
       for (int i=0; i<nlocalPhase; ++i)
         distfVector(i) = distfPtr[i];
 
-      // Compute density
-      tempNumDensity.noalias() = mom0Matrix*distfVector;
-      double originalNum = (tempNumDensity.cwiseProduct(volWeightsConf)).sum();
+      // Compute number of particles
+      double originalNum = (distfVector.cwiseProduct(volWeights)).sum();
       if (originalNum<0.0)
       {
-        //std::cout << "entire cell negative (density = " << originalNum << ")" << std::endl;
+        std::cout << "entire cell negative (density = " << originalNum << ")" << std::endl;
         for (int i=0; i<nlocalPhase; ++i)
         {
           if (distfPtr[i] < 0.0)
@@ -232,8 +135,7 @@ namespace Lucee
       }
 
       // Compute modified density. This will be greater than originalNum
-      tempNumDensity.noalias() = mom0Matrix*distfVector;
-      double modifiedNum = (tempNumDensity.cwiseProduct(volWeightsConf)).sum();
+      double modifiedNum = (distfVector.cwiseProduct(volWeights)).sum();
 
       if (modifiedNum==0.0)
       {
