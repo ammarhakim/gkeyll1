89c75dedcfc01fd929622edda9cf7643190930d9
Petr Cagas <pcagas@vt.edu>
2017-08-31 10:43:45 -0400

    LcThreeFluidCollisionSource debuged

--- proto/LcThreeFluidCollisionSource.cpp
+++ proto/LcThreeFluidCollisionSource.cpp
@@ -32,162 +32,161 @@ namespace Lucee
     if (tbl.hasNumber("phi"))
       phi = tbl.getNumber("phi");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify ionization energy using 'phi'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify ionization energy using 'phi'");
     if (tbl.hasNumber("A"))
       voronovA = tbl.getNumber("A");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify Voronov ionization fitting parameter A using 'A'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify Voronov ionization fitting parameter A using 'A'");
     if (tbl.hasNumber("P"))
       voronovP = tbl.getNumber("P");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify Voronov ionization fitting parameter P using 'P'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify Voronov ionization fitting parameter P using 'P'");
     if (tbl.hasNumber("X"))
       voronovX = tbl.getNumber("X");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify Voronov ionization fitting parameter X using 'X'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify Voronov ionization fitting parameter X using 'X'");
     if (tbl.hasNumber("K"))
       voronovK = tbl.getNumber("K");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify Voronov ionization fitting parameter K using 'K'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify Voronov ionization fitting parameter K using 'K'");
 
     if (tbl.hasNumber("massElc"))
-      massElc = tbl.getNumber("massElc");
+      mass_e = tbl.getNumber("massElc");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify electron mass using 'massElc'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify electron mass using 'massElc'");
     if (tbl.hasNumber("massIon"))
-      massIon = tbl.getNumber("massIon");
+      mass_i = tbl.getNumber("massIon");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify ion mass using 'massIon'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify ion mass using 'massIon'");
     if (tbl.hasNumber("massNeut"))
-      massIon = tbl.getNumber("massNeut");
+      mass_n = tbl.getNumber("massNeut");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify neutrals mass using 'massNeut'");
+      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify neutral mass using 'massNeut'");
 
     if (tbl.hasNumber("chargeElc"))
-      chargeElc = tbl.getNumber("chargeElc");
+      charge_e = tbl.getNumber("chargeElc");
     else
       throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify electron charge using 'chargeElc'");
     if (tbl.hasNumber("chargeIon"))
-      chargeIon = tbl.getNumber("chargeIon");
+      charge_i = tbl.getNumber("chargeIon");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify ion charge using 'chargeIon'");
-
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify ion charge using 'chargeIon'");
 
     if (tbl.hasNumber("gasGamma"))
       gasGamma = tbl.getNumber("gasGamma");
     else
-      throw Lucee::Except("ThreeFluidCollisionSource::readInput: Must specify gas gamma using 'gasGamma'");
+      throw Lucee::Except("ThreeFluidCollision::readInput: Must specify gas gamma using 'gasGamma'");
   }
 
   void
   ThreeFluidCollisionSource::getSource(double tm, const double loc[3], std::vector<double>& src)
   {
     // inputs
-    double rhoElc = this->getData(0);
-    double rhoUxElc = this->getData(1);
-    double rhoUyElc = this->getData(2);
-    double rhoUzElc = this->getData(3);
-    double ErElc = this->getData(4);
-    double rhoIon = this->getData(5);
-    double rhoUxIon = this->getData(6);
-    double rhoUyIon = this->getData(7);
-    double rhoUzIon = this->getData(8);
-    double ErIon = this->getData(9);
-    double rhoNeut = this->getData(10);
-    double rhoUxNeut = this->getData(11);
-    double rhoUyNeut = this->getData(12);
-    double rhoUzNeut = this->getData(13);
-    double ErNeut = this->getData(14);
+    double rho_e = this->getData(0);
+    double rhoUx_e = this->getData(1);
+    double rhoUy_e = this->getData(2);
+    double rhoUz_e = this->getData(3);
+    double Edens_e = this->getData(4);
+    double rho_i = this->getData(5);
+    double rhoUx_i = this->getData(6);
+    double rhoUy_i = this->getData(7);
+    double rhoUz_i = this->getData(8);
+    double Edens_i = this->getData(9);
+    double rho_n = this->getData(10);
+    double rhoUx_n = this->getData(11);
+    double rhoUy_n = this->getData(12);
+    double rhoUz_n = this->getData(13);
+    double Edens_n = this->getData(14);
 
     double elemCharge, Z;
-    double invRhoElc, invRhoIon, invRhoNeut;
-    double invMassElc, invMassIon, invMassNeut;
-    double nElc, nNeut;
-    double TElc, TIon, TNeut;
-    double EkinElc, EkinIon, EkinNeut;
-    double uxNeut, uyNeut, uzNeut;
+    double n_e, n_i, n_n;
+    double T_e, T_i, T_n;
+    double EdensK_e, EdensK_i, EdensK_n, EK_n;
+    double ux_n, uy_n, uz_n;
 
     double voronovU, ionizationRate, gammaIonization;
 
-    double tauElc;
+    double tau_e;
     double sigma;
     double jx, jy, jz;
     double coulombLog;
-    double Reix, Reiy, Reiz;
+    double Rx_ei, Ry_ei, Rz_ei;
 
-    double QnIonization, Qie, Qei;
+    double Qion_n, Q_ie, Q_ei;
 
-    elemCharge = abs(chargeElc);
-    Z = chargeIon/elemCharge;
-    invRhoElc = 1.0/rhoElc;
-    invRhoIon = 1.0/rhoIon;
-    invRhoNeut = 1.0/rhoNeut;
-    invMassElc = 1.0/massElc;
-    invMassIon = 1.0/massIon;
-    invMassNeut = 1.0/massNeut;
+    elemCharge = fabs(charge_e);
+    Z = charge_i / elemCharge;
 
-    nElc = rhoElc*invMassElc;
-    nNeut = rhoNeut*invMassNeut;
+    n_e = rho_e / mass_e;
+    n_i = rho_i / mass_i;
+    n_n = rho_n / mass_n;
 
-    EkinElc = 0.5*invRhoElc*(rhoUxElc*rhoUxElc + rhoUyElc*rhoUyElc +
-			     rhoUzElc*rhoUzElc);
-    EkinIon = 0.5*invRhoIon*(rhoUxIon*rhoUxIon + rhoUyIon*rhoUyIon +
-			     rhoUzIon*rhoUzIon);
-    EkinNeut = 0.5*invRhoNeut*(rhoUxNeut*rhoUxNeut + rhoUyNeut*rhoUyNeut +
-			       rhoUzNeut*rhoUzNeut);
+    ux_n = rhoUx_n / rho_n;
+    uy_n = rhoUy_n / rho_n;
+    uz_n = rhoUz_n / rho_n;
+
+    EdensK_e = 0.5*(rhoUx_e*rhoUx_e + rhoUy_e*rhoUy_e +
+		    rhoUz_e*rhoUz_e)/rho_e;
+    EdensK_i = 0.5*(rhoUx_i*rhoUx_i + rhoUy_i*rhoUy_i +
+		    rhoUz_i*rhoUz_i)/rho_i;
+    EdensK_n = 0.5*(rhoUx_n*rhoUx_n + rhoUy_n*rhoUy_n +
+		    rhoUz_n*rhoUz_n)/rho_n;
+    EK_n = EdensK_n*mass_n/rho_n;
     
-    TElc = (gasGamma-1)*(ErElc - EkinElc)*invRhoElc / elemCharge; // in eV
-    TIon = (gasGamma-1)*(ErIon - EkinIon)*invRhoIon / elemCharge; // in eV
-    TNeut = (gasGamma-1)*(ErNeut - EkinNeut)*invRhoNeut / elemCharge;
-    uxNeut = rhoUxNeut*invMassNeut;
-    uyNeut = rhoUyNeut*invMassNeut;
-    uzNeut = rhoUzNeut*invMassNeut;
-    voronovU = phi/TElc;
-    ionizationRate = voronovA*1e-6*(1 + voronovP*sqrt(voronovU))/(voronovX + voronovU)*pow(voronovU, voronovK)*exp(-1.0*voronovU);
-    gammaIonization = nElc*nNeut*ionizationRate;
+    // all the following formulas are for temperatures in eV but
+    // sumulations are running in SI; therefore elemCharge can
+    // unexpectedly appear in the formulas
+    T_e = (gasGamma-1)*(Edens_e - EdensK_e)/n_e / elemCharge;
+    T_i = (gasGamma-1)*(Edens_i - EdensK_i)/n_i / elemCharge;
+    T_n = (gasGamma-1)*(Edens_n - EdensK_n)/n_n / elemCharge;
 
-    if (TElc < 50) {
-      coulombLog = 23.4 - 1.15*log(nElc/1e6) + 3.45*log(TElc);
+    voronovU = phi / T_e;
+    ionizationRate = voronovA*1e-6* (1 + voronovP*sqrt(voronovU)) / (voronovX + voronovU) * pow(voronovU, voronovK) * exp(-1.0*voronovU);
+    gammaIonization = n_e*n_n*ionizationRate;
+
+    if (T_e < 50) {
+      coulombLog = 23.4 - 1.15*log(n_e*1e-6) + 3.45*log(T_e);
     }
     else {
-      coulombLog = 25.3 - 1.15*log(nElc/1e6) + 2.3*log(TElc);
+      coulombLog = 25.3 - 1.15*log(n_e*1e-6) + 2.3*log(T_e);
     }
 
-    tauElc = 3.5e11/coulombLog*sqrt(TElc*TElc*TElc)/Z * massElc*invRhoElc;
-    sigma = 1.96*nElc*elemCharge*elemCharge*tauElc*invMassElc;
-    jx = chargeElc*rhoUxElc*invMassElc + chargeIon*rhoUxIon*invMassIon;
-    jy = chargeElc*rhoUyElc*invMassElc + chargeIon*rhoUyIon*invMassIon;
-    jz = chargeElc*rhoUzElc*invMassElc + chargeIon*rhoUzIon*invMassIon;
-    Reix = elemCharge*nElc*jx/sigma;
-    Reiy = elemCharge*nElc*jy/sigma;
-    Reiz = elemCharge*nElc*jz/sigma;
+    // From Braginskii:
+    tau_e = 3.5e11/coulombLog * sqrt(T_e*T_e*T_e)/Z * mass_e/rho_e;
+    sigma = 1.96 * n_e * elemCharge*elemCharge * tau_e / mass_e;
+    jx = charge_e*rhoUx_e/mass_e + charge_i*rhoUx_i/mass_i;
+    jy = charge_e*rhoUy_e/mass_e + charge_i*rhoUy_i/mass_i;
+    jz = charge_e*rhoUz_e/mass_e + charge_i*rhoUz_i/mass_i;
+    Rx_ei = elemCharge*n_e*jx/sigma;
+    Ry_ei = elemCharge*n_e*jy/sigma;
+    Rz_ei = elemCharge*n_e*jz/sigma;
 
-    QnIonization = 1.5*gammaIonization*TNeut;
-    Qie = 3*rhoElc*invMassIon/tauElc*(TElc-TIon);
-    Qei = (jx*jx + jy*jy + jz*jz)/sigma - Qie;
+    Qion_n = 1.5*gammaIonization*T_n * elemCharge;
+    Q_ie = 3*rho_e/mass_i/tau_e*(T_e-T_i) * elemCharge;
+    Q_ei = (jx*jx + jy*jy + jz*jz)/sigma * elemCharge - Q_ie;
       
     // continuity sources
-    src[0] = massElc*gammaIonization;
-    src[5] = massIon*gammaIonization;
-    src[10] = -1.0*massNeut*gammaIonization;
+    src[0] = mass_e*gammaIonization;
+    src[5] = mass_i*gammaIonization;
+    src[10] = -mass_n*gammaIonization;
 
     // momentum sources
-    src[1] = gammaIonization*massElc*uxNeut + Reix;
-    src[2] = gammaIonization*massElc*uyNeut + Reiy;
-    src[3] = gammaIonization*massElc*uzNeut + Reiz;
-    src[6] = gammaIonization*massIon*uxNeut - Reix;
-    src[7] = gammaIonization*massIon*uyNeut - Reiy;
-    src[8] = gammaIonization*massIon*uzNeut - Reiz;
-    src[11] = -1.0*gammaIonization*rhoUxNeut;
-    src[12] = -1.0*gammaIonization*rhoUyNeut;
-    src[13] = -1.0*gammaIonization*rhoUzNeut;
+    src[1] = gammaIonization*mass_e*ux_n + Rx_ei;
+    src[2] = gammaIonization*mass_e*uy_n + Ry_ei;
+    src[3] = gammaIonization*mass_e*uz_n + Rz_ei;
+    src[6] = gammaIonization*mass_i*ux_n;// - Rx_ei;
+    src[7] = gammaIonization*mass_i*uy_n - Ry_ei;
+    src[8] = gammaIonization*mass_i*uz_n - Rz_ei;
+    src[11] = -gammaIonization*mass_n*ux_n; 
+    src[12] = -gammaIonization*mass_n*uy_n;
+    src[13] = -gammaIonization*mass_n*uz_n;
 
     // energy sources
-    src[4] = massElc*invMassNeut*(gammaIonization*EkinNeut + QnIonization)
-      + Qei; 
-    src[9] = massIon*invMassNeut*(gammaIonization*EkinNeut + QnIonization)
-      + Qie;
-    src[14] = -1.0*(gammaIonization*EkinNeut + QnIonization); 
+    src[4] = (Rx_ei*rhoUx_e + Ry_ei*rhoUy_e + Rz_ei*rhoUz_e)/rho_e + 
+      mass_e/mass_n*(gammaIonization*EK_n + Qion_n) + Q_ei; 
+    src[9] = -(Rx_ei*rhoUx_i + Ry_ei*rhoUy_i + Rz_ei*rhoUz_i)/rho_i + 
+      mass_i/mass_n*(gammaIonization*EK_n + Qion_n) + Q_ie;
+    src[14] = -(gammaIonization*EK_n + Qion_n); 
   }
 
   void
