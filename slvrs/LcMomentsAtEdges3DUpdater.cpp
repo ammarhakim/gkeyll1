c795afd721effa2fdd02a4141a1e8ecd9e714b1f
Manaure Francisquez <2220456+manauref@users.noreply.github.com>
2017-11-10 10:43:27 -0500

    "Change updaters used by 1X2V recycling runs to support MPI."

--- slvrs/LcMomentsAtEdges3DUpdater.cpp
+++ slvrs/LcMomentsAtEdges3DUpdater.cpp
@@ -104,6 +104,7 @@ namespace Lucee
     Lucee::DynVector<double>& velocityMoments = this->getOut<Lucee::DynVector<double> >(0);
 
     Lucee::Region<3, int> globalRgn = grid.getGlobalRegion();
+    Lucee::Region<3, int> localRgn = grid.getLocalRegion();
     
     Lucee::ConstFieldPtr<double> sknPtr = distfIn.createConstPtr(); // for skin-cell
     Lucee::ConstFieldPtr<double> gstPtr = distfIn.createConstPtr(); // for ghost-cell
@@ -128,94 +129,105 @@ namespace Lucee
     int ix = globalRgn.getUpper(0)-1; // right skin cell x index
     
     // Integrate along skin cell (right edge)
-    for (int js = globalRgn.getLower(1); js < globalRgn.getUpper(1); js++)
+    if (localRgn.getUpper(0) == globalRgn.getUpper(0))
     {
-      for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
+      for (int js = globalRgn.getLower(1); js < globalRgn.getUpper(1); js++)
       {
-        idx[0] = ix;
-        idx[1] = js;
-        idx[2] = iMu;
-
-        grid.setIndex(idx);
-        grid.getCentroid(cellCentroid);
-
-        // Don't want skin cell contributions for vPara < 0.0 (upwinding)
-        if (cellCentroid[1] < 0.0)
-          break;
-
-        distfIn.setPtr(sknPtr, ix, js, iMu);
-
-        // Interpolate f to surface quadrature points
-        Eigen::VectorXd fAtNodes(numNodes);
-        for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
-          fAtNodes(nodeIndex) = sknPtr[nodeIndex];
-        Eigen::VectorXd edgeQuadData = interpEdgeMatrixUpper*fAtNodes;
-
-        // Integrate v*f over entire cell using gaussian quadrature
-        double vParaMom_cell = 0.0;
-        double vPara3Mom_cell = 0.0;
-        double vParaMuMom_cell = 0.0;
-
-        for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+        for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
         {
-          double physicalV = cellCentroid[1] + gaussEdgeOrdinatesUpper(quadNodeIndex,1)*grid.getDx(1)/2.0;
-          double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesUpper(quadNodeIndex,2)*grid.getDx(2)/2.0;
+          idx[0] = ix;
+          idx[1] = js;
+          idx[2] = iMu;
 
-          vParaMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
-          vPara3Mom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
-          vParaMuMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
-        }
-        
-        vParaMom_r += vParaMom_cell;
-        vPara3Mom_r += vPara3Mom_cell;
-        vParaMuMom_r += vParaMuMom_cell;
-      }
-    }
+          grid.setIndex(idx);
+          grid.getCentroid(cellCentroid);
 
-    // Integrate along ghost cell (right edge)
-    for (int jg = globalRgn.getLower(1); jg < globalRgn.getUpper(1); jg++)
-    {
-      for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
-      {
-        idx[0] = ix+1;
-        idx[1] = jg;
-        idx[2] = iMu;
+          // Don't want skin cell contributions for vPara < 0.0 (upwinding)
+          if (cellCentroid[1] < 0.0)
+            break;
 
-        grid.setIndex(idx);
-        grid.getCentroid(cellCentroid);
+          distfIn.setPtr(sknPtr, ix, js, iMu);
 
-        // Don't want ghost cell contributions for v > 0.0 (upwinding)
-        if (cellCentroid[1] > 0.0)
-          break;
+          // Interpolate f to surface quadrature points
+          Eigen::VectorXd fAtNodes(numNodes);
+          for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
+            fAtNodes(nodeIndex) = sknPtr[nodeIndex];
+          Eigen::VectorXd edgeQuadData = interpEdgeMatrixUpper*fAtNodes;
 
-        distfIn.setPtr(gstPtr, ix+1, jg, iMu);
+          // Integrate v*f over entire cell using gaussian quadrature
+          double vParaMom_cell = 0.0;
+          double vPara3Mom_cell = 0.0;
+          double vParaMuMom_cell = 0.0;
 
-        // Interpolate f to surface quadrature points
-        Eigen::VectorXd fAtNodes(numNodes);
-        for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
-          fAtNodes(nodeIndex) = gstPtr[nodeIndex];
-        Eigen::VectorXd edgeQuadData = interpEdgeMatrixLower*fAtNodes;
+          for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+          {
+            double physicalV = cellCentroid[1] + gaussEdgeOrdinatesUpper(quadNodeIndex,1)*grid.getDx(1)/2.0;
+            double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesUpper(quadNodeIndex,2)*grid.getDx(2)/2.0;
 
-        // Integrate v*f over entire cell using gaussian quadrature
-        double vParaMom_cell = 0.0;
-        double vPara3Mom_cell = 0.0;
-        double vParaMuMom_cell = 0.0;
-
-        for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
-        {
-          double physicalV = cellCentroid[1] + gaussEdgeOrdinatesLower(quadNodeIndex,1)*grid.getDx(1)/2.0;
-          double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesLower(quadNodeIndex,2)*grid.getDx(2)/2.0;
+            vParaMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
+            vPara3Mom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
+            vParaMuMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          }
           
-          vParaMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
-          vPara3Mom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
-          vParaMuMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          vParaMom_r += vParaMom_cell;
+          vPara3Mom_r += vPara3Mom_cell;
+          vParaMuMom_r += vParaMuMom_cell;
         }
-        
-        vParaMom_r += vParaMom_cell;
-        vPara3Mom_r += vPara3Mom_cell;
-        vParaMuMom_r += vParaMuMom_cell;
+      }
+
+      // Integrate along ghost cell (right edge)
+      for (int jg = globalRgn.getLower(1); jg < globalRgn.getUpper(1); jg++)
+      {
+        for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
+        {
+          idx[0] = ix+1;
+          idx[1] = jg;
+          idx[2] = iMu;
+
+          grid.setIndex(idx);
+          grid.getCentroid(cellCentroid);
+
+          // Don't want ghost cell contributions for v > 0.0 (upwinding)
+          if (cellCentroid[1] > 0.0)
+            break;
+
+          distfIn.setPtr(gstPtr, ix+1, jg, iMu);
+
+          // Interpolate f to surface quadrature points
+          Eigen::VectorXd fAtNodes(numNodes);
+          for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
+            fAtNodes(nodeIndex) = gstPtr[nodeIndex];
+          Eigen::VectorXd edgeQuadData = interpEdgeMatrixLower*fAtNodes;
+
+          // Integrate v*f over entire cell using gaussian quadrature
+          double vParaMom_cell = 0.0;
+          double vPara3Mom_cell = 0.0;
+          double vParaMuMom_cell = 0.0;
+
+          for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+          {
+            double physicalV = cellCentroid[1] + gaussEdgeOrdinatesLower(quadNodeIndex,1)*grid.getDx(1)/2.0;
+            double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesLower(quadNodeIndex,2)*grid.getDx(2)/2.0;
+            
+            vParaMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
+            vPara3Mom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
+            vParaMuMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          }
+          
+          vParaMom_r += vParaMom_cell;
+          vPara3Mom_r += vPara3Mom_cell;
+          vParaMuMom_r += vParaMuMom_cell;
+        }
       }
     }
+#ifdef HAVE_MPI
+    TxCommBase *zComm = grid.getComm();
+    int mpi_size = zComm->getNumProcs();
+    // Presumably the left edge is held in the rank=0 MPI process.
+    zComm->bcast(1,&vParaMom_r,mpi_size-1);
+    zComm->bcast(1,&vPara3Mom_r,mpi_size-1);
+    zComm->bcast(1,&vParaMuMom_r,mpi_size-1);
+#endif
 
     //<vPara*f>
     double vParaMom_l = 0.0;
@@ -227,95 +239,104 @@ namespace Lucee
     ix = globalRgn.getLower(0); // left skin cell x index
     
     // Integrate along skin cell (left edge)
-    for (int js = globalRgn.getLower(1); js < globalRgn.getUpper(1); js++)
+    if (localRgn.getLower(0) == globalRgn.getLower(0))
     {
-      for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
+      for (int js = globalRgn.getLower(1); js < globalRgn.getUpper(1); js++)
       {
-        idx[0] = ix;
-        idx[1] = js;
-        idx[2] = iMu;
-
-        grid.setIndex(idx);
-        grid.getCentroid(cellCentroid);
-
-        // Don't want skin cell contributions for vPara > 0.0 (upwinding)
-        if (cellCentroid[1] > 0.0)
-          break;
-
-        distfIn.setPtr(sknPtr, ix, js, iMu);
-
-        // Interpolate f to surface quadrature points
-        Eigen::VectorXd fAtNodes(numNodes);
-        for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
-          fAtNodes(nodeIndex) = sknPtr[nodeIndex];
-        Eigen::VectorXd edgeQuadData = interpEdgeMatrixLower*fAtNodes;
-
-        // Integrate v*f over entire cell using gaussian quadrature
-        double vParaMom_cell = 0.0;
-        double vPara3Mom_cell = 0.0;
-        double vParaMuMom_cell = 0.0;
-
-        for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+        for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
         {
-          double physicalV = cellCentroid[1] + gaussEdgeOrdinatesLower(quadNodeIndex,1)*grid.getDx(1)/2.0;
-          double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesLower(quadNodeIndex,2)*grid.getDx(2)/2.0;
+          idx[0] = ix;
+          idx[1] = js;
+          idx[2] = iMu;
 
-          vParaMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
-          vPara3Mom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
-          vParaMuMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
-        }
-        
-        vParaMom_l += vParaMom_cell;
-        vPara3Mom_l += vPara3Mom_cell;
-        vParaMuMom_l += vParaMuMom_cell;
-      }
-    }
+          grid.setIndex(idx);
+          grid.getCentroid(cellCentroid);
 
-    // Integrate along ghost cell (left edge)
-    for (int jg = globalRgn.getLower(1); jg < globalRgn.getUpper(1); jg++)
-    {
-      for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
-      {
-        idx[0] = ix-1;
-        idx[1] = jg;
-        idx[2] = iMu;
+          // Don't want skin cell contributions for vPara > 0.0 (upwinding)
+          if (cellCentroid[1] > 0.0)
+            break;
 
-        grid.setIndex(idx);
-        grid.getCentroid(cellCentroid);
+          distfIn.setPtr(sknPtr, ix, js, iMu);
 
-        // Don't want ghost cell contributions for v < 0.0 (upwinding)
-        if (cellCentroid[1] < 0.0)
-          break;
+          // Interpolate f to surface quadrature points
+          Eigen::VectorXd fAtNodes(numNodes);
+          for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
+            fAtNodes(nodeIndex) = sknPtr[nodeIndex];
+          Eigen::VectorXd edgeQuadData = interpEdgeMatrixLower*fAtNodes;
 
-        distfIn.setPtr(gstPtr, ix-1, jg, iMu);
+          // Integrate v*f over entire cell using gaussian quadrature
+          double vParaMom_cell = 0.0;
+          double vPara3Mom_cell = 0.0;
+          double vParaMuMom_cell = 0.0;
 
-        // Interpolate f to surface quadrature points
-        Eigen::VectorXd fAtNodes(numNodes);
-        for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
-          fAtNodes(nodeIndex) = gstPtr[nodeIndex];
-        Eigen::VectorXd edgeQuadData = interpEdgeMatrixUpper*fAtNodes;
+          for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+          {
+            double physicalV = cellCentroid[1] + gaussEdgeOrdinatesLower(quadNodeIndex,1)*grid.getDx(1)/2.0;
+            double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesLower(quadNodeIndex,2)*grid.getDx(2)/2.0;
 
-        // Integrate v*f over entire cell using gaussian quadrature
-        double vParaMom_cell = 0.0;
-        double vPara3Mom_cell = 0.0;
-        double vParaMuMom_cell = 0.0;
-
-        for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
-        {
-          double physicalV = cellCentroid[1] + gaussEdgeOrdinatesUpper(quadNodeIndex,1)*grid.getDx(1)/2.0;
-          double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesUpper(quadNodeIndex,2)*grid.getDx(2)/2.0;
+            vParaMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
+            vPara3Mom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
+            vParaMuMom_cell += gaussEdgeWeightsLower[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          }
           
-          vParaMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
-          vPara3Mom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
-          vParaMuMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          vParaMom_l += vParaMom_cell;
+          vPara3Mom_l += vPara3Mom_cell;
+          vParaMuMom_l += vParaMuMom_cell;
         }
+      }
 
-        vParaMom_l += vParaMom_cell;
-        vPara3Mom_l += vPara3Mom_cell;
-        vParaMuMom_l += vParaMuMom_cell;
+      // Integrate along ghost cell (left edge)
+      for (int jg = globalRgn.getLower(1); jg < globalRgn.getUpper(1); jg++)
+      {
+        for (int iMu = globalRgn.getLower(2); iMu < globalRgn.getUpper(2); iMu++)
+        {
+          idx[0] = ix-1;
+          idx[1] = jg;
+          idx[2] = iMu;
+
+          grid.setIndex(idx);
+          grid.getCentroid(cellCentroid);
+
+          // Don't want ghost cell contributions for v < 0.0 (upwinding)
+          if (cellCentroid[1] < 0.0)
+            break;
+
+          distfIn.setPtr(gstPtr, ix-1, jg, iMu);
+
+          // Interpolate f to surface quadrature points
+          Eigen::VectorXd fAtNodes(numNodes);
+          for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++)
+            fAtNodes(nodeIndex) = gstPtr[nodeIndex];
+          Eigen::VectorXd edgeQuadData = interpEdgeMatrixUpper*fAtNodes;
+
+          // Integrate v*f over entire cell using gaussian quadrature
+          double vParaMom_cell = 0.0;
+          double vPara3Mom_cell = 0.0;
+          double vParaMuMom_cell = 0.0;
+
+          for (int quadNodeIndex = 0; quadNodeIndex < edgeQuadData.rows(); quadNodeIndex++)
+          {
+            double physicalV = cellCentroid[1] + gaussEdgeOrdinatesUpper(quadNodeIndex,1)*grid.getDx(1)/2.0;
+            double physicalMu = cellCentroid[2] + gaussEdgeOrdinatesUpper(quadNodeIndex,2)*grid.getDx(2)/2.0;
+            
+            vParaMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*edgeQuadData(quadNodeIndex);
+            vPara3Mom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalV*physicalV*edgeQuadData(quadNodeIndex);
+            vParaMuMom_cell += gaussEdgeWeightsUpper[quadNodeIndex]*physicalV*physicalMu*edgeQuadData(quadNodeIndex);
+          }
+
+          vParaMom_l += vParaMom_cell;
+          vPara3Mom_l += vPara3Mom_cell;
+          vParaMuMom_l += vParaMuMom_cell;
+        }
       }
     }
 
+#ifdef HAVE_MPI
+    // Presumably the left edge is held in the rank=0 MPI process.
+    zComm->bcast(1,&vParaMom_l,0);
+    zComm->bcast(1,&vPara3Mom_l,0);
+    zComm->bcast(1,&vParaMuMom_l,0);
+#endif
 
 
     std::vector<double> data(6);
