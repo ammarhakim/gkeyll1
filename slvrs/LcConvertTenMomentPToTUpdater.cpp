8f61fe4f3925fda641d605e7b6fda79eef0fed0c
Jonathan Ng <jonngwx@gmail.com>
2018-12-19 06:45:27 -0800

    anisotropic conductivity approximation for pressure tensor

--- slvrs/LcConvertTenMomentPToTUpdater.cpp
+++ slvrs/LcConvertTenMomentPToTUpdater.cpp
@@ -34,7 +34,20 @@ namespace Lucee
       lce << nf << " not recognized!" << std::endl;
       throw lce;
     }
-    T0 = 0.0;
+
+    outType = OUT_TEMPERATURE;
+    if (tbl.hasString("outType")) { 
+      std::string outQty = tbl.getString("outType");
+      if (outQty == "OutPressure"){
+        outType = OUT_PRESSURE;
+      } else if (outQty == "OutTemperature") {
+        outType = OUT_TEMPERATURE;
+      } else {
+        Lucee::Except lce("ConvertPToTUpdater::readInput: 'outType' ");
+        lce << outQty << " not recognized!" << std::endl;
+        throw lce;
+      }
+    }
   }
 
   template <unsigned NDIM>
@@ -70,29 +83,46 @@ namespace Lucee
         double u = ptr[1]/r;
         double v = ptr[2]/r;
         double w = ptr[3]/r;
-        ptr[4] = ptr[4]/r-u*u;
-        ptr[5] = ptr[5]/r-u*v;
-        ptr[6] = ptr[6]/r-u*w;
-        ptr[7] = ptr[7]/r-v*v;
-        ptr[8] = ptr[8]/r-v*w;
-        ptr[9] = ptr[9]/r-w*w;
-        //        T0 = (ptr[4] + ptr[7] + ptr[9])/3.0*0.0;
-        //        ptr[4] = ptr[4] - T0;
-        //        ptr[7] = ptr[7] - T0;
-        //        ptr[9] = ptr[9] - T0;
-        //meanPtr[0] = T0;
+        // this converts pressure to temperature
+        if (outType == OUT_TEMPERATURE) {
+          ptr[4] = ptr[4]/r-u*u;
+          ptr[5] = ptr[5]/r-u*v;
+          ptr[6] = ptr[6]/r-u*w;
+          ptr[7] = ptr[7]/r-v*v;
+          ptr[8] = ptr[8]/r-v*w;
+          ptr[9] = ptr[9]/r-w*w;
+        } else {
+          // convert to pressure
+          ptr[4] = ptr[4]-u*u*r;
+          ptr[5] = ptr[5]-u*v*r;
+          ptr[6] = ptr[6]-u*w*r;
+          ptr[7] = ptr[7]-v*v*r;
+          ptr[8] = ptr[8]-v*w*r;
+          ptr[9] = ptr[9]-w*w*r;       
+        }
       } else {
         double r = ptr[0];
         double u = ptr[1]/r;
         double v = ptr[2]/r;
         double w = ptr[3]/r;
         //        T0 = meanPtr[0];
-        ptr[4] = (ptr[4])*r+r*u*u;
-        ptr[5] = ptr[5]*r+r*u*v;
-        ptr[6] = ptr[6]*r+r*u*w;
-        ptr[7] = (ptr[7])*r+r*v*v;
-        ptr[8] = ptr[8]*r+r*v*w;
-        ptr[9] = (ptr[9])*r+r*w*w;
+        if (outType == OUT_TEMPERATURE) {
+          // given temperature, convert to P + rho u u
+          ptr[4] = ptr[4]*r+r*u*u;
+          ptr[5] = ptr[5]*r+r*u*v;
+          ptr[6] = ptr[6]*r+r*u*w;
+          ptr[7] = ptr[7]*r+r*v*v;
+          ptr[8] = ptr[8]*r+r*v*w;
+          ptr[9] = ptr[9]*r+r*w*w;
+        } else {
+          // give P, convert to P + rho u u 
+          ptr[4] = ptr[4] + r*u*u;
+          ptr[5] = ptr[5] + r*u*v;
+          ptr[6] = ptr[6] + r*u*w;
+          ptr[7] = ptr[7] + r*v*v;
+          ptr[8] = ptr[8] + r*v*w;
+          ptr[9] = ptr[9] + r*w*w;
+        }
       }
     }
     
